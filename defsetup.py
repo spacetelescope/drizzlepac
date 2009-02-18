426fe554625bd057674b700cde8aa66354029cce
hack <hack@stsci.edu>
2009-02-18 15:47:48 +0000

    Modified the 'defsetup.py' module to build the C drizzle code upon installation. This build, though, builds against a private version of CFITSIO, not a generally available library. WJH
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/development/trunk/BigBlackBox@7595 fe389314-cf27-0410-b35b-8c050e845b92

--- defsetup.py
+++ defsetup.py
@@ -1,3 +1,58 @@
+from distutils.core import Extension
+import sys, os.path
+from distutils import sysconfig
+
+try:
+    import numpy
+    import numpy.numarray as nn
+except ImportError:
+    "Numpy was not found. It may not be installed or it may not be on your PYTHONPATH. Pydrizzle requires numpy v 1.0.2 or later.\n"
+
+if numpy.__version__ < "1.0.2":
+    raise SystemExit, "Numpy 1.0.2 or later required to build pydrizzle."
+
+print "Building C extensions using NUMPY."
+
+numpyinc = numpy.get_include()
+numpynumarrayinc = nn.get_numarray_include_dirs()
+
+pythonlib = sysconfig.get_python_lib(plat_specific=1)
+pythoninc = sysconfig.get_python_inc()
+ver = sysconfig.get_python_version()
+pythonver = 'python' + ver
+
+cfitsioinc = []
+
+if sys.platform != 'win32':
+    pydrizzle_libraries = ['m']
+    cfitsioinc = ['/user/dencheva/mdriz-dev/soft-build/cfitsio']
+    EXTRA_LINK_ARGS = ['-L/user/dencheva/mdriz-dev/soft-build/cfitsio']
+else:
+    pydrizzle_libraries = []
+    EXTRA_LINK_ARGS = ['/NODEFAULTLIB:MSVCRT']
+
+
+ 
+def getNumpyExtensions():
+    ext = [Extension("pydrizzle.arrdriz",['src/arrdrizmodule.c',
+                                          'src/cdrizzleblot.c',
+                                          'src/cdrizzlebox.c',
+                                          'src/cdrizzleio.c',
+                                          'src/cdrizzlemap.c',
+                                          'src/cdrizzleutil.c',
+                                          'src/cdrizzlewcs.c'],
+                     define_macros=[('NUMPY', '1')],
+                     # undef_macros=['NDEBUG'],
+                     include_dirs=[pythoninc] + [numpyinc]+ cfitsioinc + numpynumarrayinc,
+                     # library_dirs=[],
+                     extra_link_args=EXTRA_LINK_ARGS,
+                     libraries=['m', 'cfitsio'],
+                     extra_compile_args=['-funroll-loops', '-DPYDRIZZLE'] # , '-fno-inline', '-O0']
+                     )]
+
+    return ext
+
+
 pkg = "BigBlackBox"
 
 setupargs = {
@@ -9,6 +64,6 @@ setupargs = {
     'license' :         "http://www.stsci.edu/resources/software_hardware/pyraf/LICENSE",
     'platforms' :       ["Linux","Solaris","Mac OS X","Win"],
     'scripts' :         [] ,
-                    
+    'ext_modules' :     getNumpyExtensions()
     }
 
