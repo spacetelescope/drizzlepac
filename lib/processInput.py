841b64cc819b3bd38bf875a2c63d1f37d98fbc01
hack <hack@stsci.edu>
2011-02-02 19:04:10 +0000

    Corrected a logic problem in 'processInput.createImageObjectList()' dealing that identifies whether or not an image is for a moving target.  The "MTFLAG" keyword is not always present, so this code only gets triggered now if the keyword is present.
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/betadrizzle@11831 fe389314-cf27-0410-b35b-8c050e845b92

--- lib/processInput.py
+++ lib/processInput.py
@@ -155,8 +155,9 @@ def createImageObjectList(files,instrpars,group=None):
     for img in files:
         image = _getInputImage(img,group=group)
         image.setInstrumentParameters(instrpars)
-        # check to see whether we are dealing with moving target observations...
-        mtflag = image._image['PRIMARY'].header['MTFLAG']
+        if image._image['PRIMARY'].header.has_key('MTFLAG'):
+            # check to see whether we are dealing with moving target observations...
+            mtflag = image._image['PRIMARY'].header['MTFLAG']
         if mtflag:
             if mt_refimg is None:
                 mt_refimg = image
