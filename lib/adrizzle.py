0b09acd4a7d81867948760a6157e2f7da906cba0
hack <hack@stsci.edu>
2011-02-17 15:25:11 +0000

    Another revision to finally fix the problem with 'fillval' parameter values other than blank ('').
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/betadrizzle@11960 fe389314-cf27-0410-b35b-8c050e845b92

--- lib/adrizzle.py
+++ lib/adrizzle.py
@@ -778,6 +778,8 @@ def do_driz(insci, input_wcs, inwht,
     # Insure that the fillval parameter gets properly interpreted for use with tdriz
     if util.is_blank(fillval):
         fillval = 'INDEF'
+    else:
+        fillval = str(fillval)
 
     if in_units == 'cps':
         expscale = 1.0
