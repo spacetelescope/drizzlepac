c70e33e07733bf6e48b9424fa8332a85b28e0722
cslocum <cslocum@stsci.edu>
2014-04-29 16:33:45 +0000

    create setup hook for drizzlepac that will allow it to find the astropy.wcs include dir
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/drizzlepac/trunk@31246 fe389314-cf27-0410-b35b-8c050e845b92

---
+++ lib/drizzlepac/hooks.py
@@ -0,0 +1,38 @@
+
+'''
+To compile another package that wants to link with the C code in astropy.wcs:
+
+[build_ext]
+pre-hook.wcs = drizzlepac.hooks.setup
+
+[extension=drizzlepac.cdriz]
+include_dirs = numpy wcs
+libraries = wcs m
+
+'''
+
+
+import os
+
+def setup(command_obj) :
+
+    # warning if we somehow get called when not building a c extension
+    command_name = command_obj.get_command_name()
+    if command_name != 'build_ext':
+        log.warn('%s is meant to be used with the build_ext command only; '
+                 'it is not for use with the %s command.' %
+                 (__name__, command_name))
+
+    # get information from astropy.wcs
+    from astropy import wcs
+	includes = wcs.get_include()
+
+    # each place where the include directory is named exactly "pywcs",
+    # replace it with the include directories computed above
+    for extension in command_obj.extensions:
+        if 'wcs' not in extension.include_dirs:
+            continue
+        idx = extension.include_dirs.index('wcs')
+        for inc in includes:
+            extension.include_dirs.insert(idx, inc)
+        extension.include_dirs.remove('wcs')
