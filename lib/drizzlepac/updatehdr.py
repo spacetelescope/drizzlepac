5711d5edaf2aef431ba28173f20441bf25dc4d47
hack <hack@stsci.edu>
2014-11-18 19:20:36 +0000

    The formatting assumed for reading in shiftfiles did not account for filenames >24 characters long, such as those used for default single_sci files.  This logic was revised to make the code more robust for users running it on their data.
    Version Date was also updated to reflect the latest status.
    
    
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/drizzlepac/trunk@35535 fe389314-cf27-0410-b35b-8c050e845b92

--- lib/drizzlepac/updatehdr.py
+++ lib/drizzlepac/updatehdr.py
@@ -64,14 +64,21 @@ def update_from_shiftfile(shiftfile,wcsname=None,force=False):
             refimage = refimage[:refimage.find('[wcs]')]
             break
 
+    # Determine the max length in the first column (filenames)
+    fnames = []
+    for row in shift_lines:
+        if row.startswith('#'): continue
+        fnames.append(len(row.split(' ')[0]))
+    fname_fmt = 'S{0}'.format(max(fnames))
+
     # Now read in numerical values from shiftfile
     type_list = {'names':('fnames','xsh','ysh','rot','scale','xrms','yrms'),
-                 'formats':('S24','f4','f4','f4','f4','f4','f4')}
+                 'formats':(fname_fmt,'f4','f4','f4','f4','f4','f4')}
     try:
         sdict = np.loadtxt(shiftfile,dtype=type_list,unpack=False)
     except IndexError:
         tlist = {'names':('fnames','xsh','ysh','rot','scale'),
-                     'formats':('S24','f4','f4','f4','f4')}
+                     'formats':(fname_fmt,'f4','f4','f4','f4')}
         s = np.loadtxt(shiftfile,dtype=tlist,unpack=False)
         sdict = np.zeros([s['fnames'].shape[0],],dtype=type_list)
         for sname in s.dtype.names:
@@ -383,8 +390,6 @@ def update_refchip_with_shift(chip_wcs, wcslin, fitgeom='rscale',
         if err < maxUerr:
             break
         err0 = err
-
-    #print("|u - crpix|: {}".format(np.linalg.norm(u - chip_wcs.wcs.crpix)))
 
     if xrms is not None:
         chip_wcs.wcs.crder = np.array([xrms,yrms])
