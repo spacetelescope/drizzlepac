spacetelescope/drizzlepac

commit 894bf0e9a085865867ab82b38dec9803aa8021c2

Removing `updatwcs` parameter from TEAL interface for `AstroDrizzle` and `TweakReg`. See ticket #1217 for more details.
git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/drizzlepac/trunk@43005 fe389314-cf27-0410-b35b-8c050e845b92

lib/drizzlepac/processInput.py
@@ -96,6 +96,10 @@ def setCommonInput(configObj, createOutwcs=True):
 
 
     """
+    # make sure 'updatewcs' is set to False when running from GUI or if missing
+    # from configObj:
+    if 'updatewcs' not in configobj:
+        configobj['updatewcs'] = False
 
     if not createOutwcs or not configObj['coeffs']:
         # we're probably just working on single images here

commit 26970b73d0f1765f9058ba9e9f23db87a32b0097

Fix a bug introduced in r43005 (ticket #1217) in which `configobj` should have been `configObj`.
git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/drizzlepac/trunk@43230 fe389314-cf27-0410-b35b-8c050e845b92

lib/drizzlepac/processInput.py
@@ -78,8 +78,7 @@ def setCommonInput(configObj, createOutwcs=True):
     Notes
     -----
     At a minimum, the configObj instance (dictionary) should contain:
-        configObj = {'input':None,'output':None,
-                    'updatewcs':None}
+        configObj = {'input':None,'output':None }
 
     If provided, the configObj should contain the values of all the multidrizzle parameters
     as set by the user with TEAL. If no configObj is given, it will retrieve
@@ -98,8 +97,8 @@ def setCommonInput(configObj, createOutwcs=True):
     """
     # make sure 'updatewcs' is set to False when running from GUI or if missing
     # from configObj:
-    if 'updatewcs' not in configobj:
-        configobj['updatewcs'] = False
+    if 'updatewcs' not in configObj:
+        configObj['updatewcs'] = False
 
     if not createOutwcs or not configObj['coeffs']:
         # we're probably just working on single images here