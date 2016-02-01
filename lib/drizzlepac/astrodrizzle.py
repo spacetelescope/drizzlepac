spacetelescope/drizzlepac

commit 0d624163856cd5c07a18f1366cf4910636bb59d9

Some updates needed for DrizzlePac based on errors found during HSTDP 2015.3 testing; namely,   - string.letters changed to string.ascii_letters which is common in both Python 2.x and 3.x   - astrodrizzle needed to process configobj='defaults' correctly to load system configobj parameter values when running with MDRIZTAB in pipeline mode
git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/drizzlepac/trunk@47110 fe389314-cf27-0410-b35b-8c050e845b92

lib/drizzlepac/astrodrizzle.py
@@ -87,7 +87,7 @@ def AstroDrizzle(input=None, mdriztab=False, editpars=False, configobj=None,
             raise RuntimeError('Cannot find .cfg file: '+configobj)
         configobj = teal.load(configobj, strict=False)
 
-    if configobj is None:
+    if configobj is None or configobj == 'defaults':
         configobj = teal.load(__taskname__)
 
     if 'updatewcs' in input_dict: # user trying to explicitly turn on updatewcs

lib/drizzlepac/mdzhandler.py
@@ -16,7 +16,6 @@ import numpy as np
 
 from stsci.tools import fileutil
 
-
 def getMdriztabParameters(files):
     """ Gets entry in MDRIZTAB where task parameters live.
         This method returns a record array mapping the selected
@@ -181,7 +180,7 @@ def cleanBlank(value):
 def findFormat(format):
     # Parses record array format string for type
     _fmt = None
-    for ltr in string.letters:
+    for ltr in string.ascii_letters:
         if format.find(ltr) > -1:
             _fmt = ltr
             break

lib/drizzlepac/tweakutils.py
@@ -283,7 +283,7 @@ def radec_hmstodd(ra,dec):
         astropy.coordinates
 
     """
-    hmstrans = string.maketrans(string.letters,' '*len(string.letters))
+    hmstrans = string.maketrans(string.ascii_letters,' '*len(string.ascii_letters))
 
     if isinstance(ra,list):
         rastr = ':'.join(ra)

commit 698196cea63b98597661f81ffde7df7796a8a04c

Moved check for user-specified input parameters for single and final wcs from util.py to astrodrizzle.py.  This avoids calling this task specific logic from within tweakreg, a bug introduced in r47110 and r47111.
git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/drizzlepac/trunk@48901 fe389314-cf27-0410-b35b-8c050e845b92

lib/drizzlepac/astrodrizzle.py
@@ -103,6 +103,12 @@ def AstroDrizzle(input=None, mdriztab=False, editpars=False, configobj=None,
         configObj = util.getDefaultConfigObj(__taskname__, configobj,
                                              input_dict,
                                              loadOnly=(not editpars))
+        print("INPUT_DICT: {}".format(input_dict))
+        # If user specifies optional parameter for final_wcs specification in input_dict,
+        #    insure that the final_wcs step gets turned on
+        util.applyUserPars_steps(configObj, input_dict, step='3a')
+        util.applyUserPars_steps(configObj, input_dict, step='7a')
+
     except ValueError:
         print("Problem with input parameters. Quitting...", file=sys.stderr)
         return

lib/drizzlepac/util.py
@@ -699,11 +699,6 @@ def getDefaultConfigObj(taskname,configObj,input_dict={},loadOnly=True):
         # the code should stop
         validateUserPars(configObj,input_dict)
 
-        # If user specifies optional parameter for final_wcs specification in input_dict,
-        #    insure that the final_wcs step gets turned on
-        applyUserPars_steps(configObj, input_dict, step='3a')
-        applyUserPars_steps(configObj, input_dict, step='7a')
-
         # If everything looks good, merge user inputs with configObj and continue
         cfgpars.mergeConfigObj(configObj, input_dict)
         # Update the input .cfg file with the updated parameter values