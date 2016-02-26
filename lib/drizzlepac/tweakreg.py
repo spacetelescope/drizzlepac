eb0457a53b53ae31233d7d1b318bfe30ca95173d
mcara <mcara@stsci.edu>
2016-02-26 00:09:49 +0000

    Add suport in `TweakReg` for exclusion files to be in different directory from input images. Also, add support for relative path specification for exclusion masks. See ticket #1253.
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/drizzlepac/trunk@49359 fe389314-cf27-0410-b35b-8c050e845b92

--- lib/drizzlepac/tweakreg.py
+++ lib/drizzlepac/tweakreg.py
@@ -178,21 +178,52 @@ def run(configobj):
         else:
             use_catfile = False
 
-    if 'exclusions' in configobj and \
-        configobj['exclusions'] not in [None,'',' ','INDEF']:
-        if os.path.exists(configobj['exclusions']):
-            #TODO: It may be useful to check that the image file names in the exclusions
-            # catalog are the same as the names specified in filenames.
-            # this way it will be easier to detect and report an incorrect exclusions file (format)
-            exclusion_files,exclusion_dict = tweakutils.parse_atfile_cat(
-                '@'+configobj['exclusions'])
+        if 'exclusions' in configobj and \
+            configobj['exclusions'] not in [None,'',' ','INDEF']:
+            if os.path.exists(configobj['exclusions']):
+                excl_files, excl_dict = tweakutils.parse_atfile_cat(
+                    '@'+configobj['exclusions'])
+
+                # make sure the dictionary is well formed and that keys are base
+                # file names and that exclusion files have been expanded:
+                exclusion_files = []
+                exclusion_dict = {}
+                rootpath = os.path.abspath(
+                    os.path.split(configobj['exclusions'])[0]
+                )
+
+                for f in excl_dict.keys():
+                    print(f)
+                    bf = os.path.basename(f)
+                    exclusion_files.append(bf)
+                    reglist = excl_dict[f]
+
+                    if reglist is None:
+                        exclusion_dict[bf] = None
+                        continue
+
+                    new_reglist = []
+                    for regfile in reglist:
+                        if regfile in [ None, 'None', '', ' ', 'INDEF' ]:
+                            new_reglist.append(None)
+                        else:
+                            abs_regfile = os.path.normpath(
+                                os.path.join(rootpath, regfile)
+                            )
+                            new_reglist.append(abs_regfile)
+
+                    exclusion_dict[bf] = new_reglist
+
+            else:
+                raise IOError('Could not find specified exclusions file "{:s}"'
+                      .format(configobj['exclusions']))
+
         else:
-            print('Could not find specified exclusions file "%s"'%(configobj['exclusions']))
-            raise IOError
-    else:
-        exclusion_files = [None]*len(filenames)
-        exclusion_dict = {}
-        for f in filenames: exclusion_dict[f] = None
+            exclusion_files = [None]*len(filenames)
+            exclusion_dict = {}
+
+            for f in filenames:
+                exclusion_dict[os.path.basename(f)] = None
 
     # Verify that we have the same number of catalog files as input images
     if catnames is not None and (len(catnames) > 0):
@@ -256,7 +287,7 @@ def run(configobj):
         for imgnum in range(len(filenames)):
             # Create Image instances for all input images
             try:
-                regexcl = exclusion_dict[filenames[imgnum]]
+                regexcl = exclusion_dict[os.path.basename(filenames[imgnum])]
             except KeyError:
                 regexcl = None
                 pass
