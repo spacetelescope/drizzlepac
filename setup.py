df9f5e861cbb13bb86a415586797cc894a9c7767
Joseph Hunkeler <jhunkeler@gmail.com>
2017-04-21 10:42:09 -0400

    Tidy up pep8

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 import os
 import numpy
-import pandokia
 import subprocess
 import sys
+
+try:
+    import pandokia
+except (NameError, ModuleNotFoundError):
+    pandokia = False
+
 from glob import glob
 from astropy import wcs
 from setuptools import setup, find_packages, Extension
@@ -18,41 +23,39 @@ else:
     except ImportError:
         try:
             subprocess.check_call(['git', 'clone',
-                'https://github.com/jhunkeler/relic.git'])
+                                   'https://github.com/jhunkeler/relic.git'])
             sys.path.insert(1, 'relic')
             import relic.release
         except subprocess.CalledProcessError as e:
             print(e)
             exit(1)
 
-
+NAME = 'drizzlepac'
 version = relic.release.get_info()
-relic.release.write_template(version, 'drizzlepac')
-
-numpy_includes = numpy.get_include()
-wcs_includes = [
-    os.path.join(wcs.get_include(), 'astropy_wcs/'),
-    os.path.join(wcs.get_include(), 'wcslib/')
-]
-fctx_includes = [
-    os.path.join(os.path.dirname(pandokia.__file__),
-                'runners', 'maker')
-]
+relic.release.write_template(version, NAME)
 
 include_dirs = []
-include_dirs.append(numpy_includes)
-include_dirs.append(fctx_includes)
-[ include_dirs.append(i) for i in wcs_includes ]
+numpy_includes = [numpy.get_include()]
+wcs_includes = [os.path.join(wcs.get_include(), 'astropy_wcs'),
+                os.path.join(wcs.get_include(), 'wcslib')]
+
+include_dirs.extend(numpy_includes)
+include_dirs.extend(wcs_includes)
+
+if pandokia:
+    fctx_includes = [os.path.join(os.path.dirname(pandokia.__file__),
+                                  'runners', 'maker')]
+    include_dirs.extend(fctx_includes)
 
 
 setup(
-    name = 'drizzlepac',
-    version = version.pep386,
-    author = 'Megan Sosey, Warren Hack, Christopher Hanley, Chris Sontag, Mihai Cara',
-    author_email = 'help@stsci.edu',
-    description = 'drizzle tools: combines astronomical images, including modeling distortion, removing cosmic rays, and generally improving fidelity of data in the final image',
-    url = 'https://github.com/spacetelescope/drizzlepac',
-    classifiers = [
+    name=NAME,
+    version=version.pep386,
+    author='Megan Sosey, Warren Hack, Christopher Hanley, Chris Sontag, Mihai Cara',
+    author_email='help@stsci.edu',
+    description='drizzle tools: combines astronomical images, including modeling distortion, removing cosmic rays, and generally improving fidelity of data in the final image',
+    url='https://github.com/spacetelescope/drizzlepac',
+    classifiers=[
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
@@ -60,7 +63,7 @@ setup(
         'Topic :: Scientific/Engineering :: Astronomy',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    install_requires = [
+    install_requires=[
         'astropy',
         'fitsblender',
         'nictools',
@@ -79,14 +82,14 @@ setup(
         'stwcs',
     ],
 
-    packages = find_packages(),
-    package_data = {
+    packages=find_packages(),
+    package_data={
         '': [
             'pars/*',
             '*.help',
         ]
     },
-    entry_points = {
+    entry_points={
         'console_scripts': [
             'mdriz=drizzlepac.mdriz:main',
             'resetbits=drizzlepac.resetbits:main',
@@ -96,8 +99,8 @@ setup(
     },
     ext_modules=[
         Extension('drizzlepac.cdriz',
-            glob('src/*.c'),
-            include_dirs=include_dirs,
-            define_macros=[('PYDRIZZLE','1')]),
+                  glob('src/*.c'),
+                  include_dirs=include_dirs,
+                  define_macros=[('PYDRIZZLE', '1')]),
     ],
 )
