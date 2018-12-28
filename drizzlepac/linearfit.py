Implement correct formula for fit_xy (#202)

drizzlepac/linearfit.py
@@ -163,7 +163,8 @@ def iter_fit_all(xy,uv,xyindx,uvindx,
     fit['ref_indx'] = uvindx
     fit['img_orig_xy'] = xyorig
     fit['ref_orig_xy'] = uvorig
-    fit['fit_xy'] = np.dot(xy, fit['fit_matrix'])+center - fit['offset']
+    fit['fit_xy'] = np.dot(xy - fit['offset'],
+                           np.linalg.inv(fit['fit_matrix'])) + center
 
     return fit
 