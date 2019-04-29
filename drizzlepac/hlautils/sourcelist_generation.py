ddf7299348663be41c7015ed17f3736e18045489
Michael Dulude <dulude@stsci.edu>
2019-04-29 15:50:46 -0400

    drizzlepac/hlautils/sourcelist_generation.py: Created placeholder-subroutines for creation of daophot-like sourcelists and source extractor-like sourcelists.

--- drizzlepac/hlautils/sourcelist_generation.py
+++ drizzlepac/hlautils/sourcelist_generation.py
@@ -11,6 +11,8 @@ __taskname__ = 'sourcelist_generation'
 
 log = logutil.create_logger(__name__, level=logutil.logging.INFO, stream=sys.stdout)
 
+# ----------------------------------------------------------------------------------------------------------------------
+
 def create_sourcelists():
     """Make sourcelists
 
@@ -22,3 +24,38 @@ def create_sourcelists():
     """
 
     log.info("SOURCELIST CREATION OCCURS HERE!")
+
+    create_daophot_like_sourcelists()
+
+    create_se_like_sourcelists()
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def create_daophot_like_sourcelists():
+    """Make daophot-like sourcelists
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+    """
+
+    log.info("DAOPHOT-LIKE SOURCELIST CREATION OCCURS HERE!")
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def create_se_like_sourcelists():
+    """Make source extractor-like sourcelists
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+    """
+
+    log.info("SOURCE EXTRACTOR-LIKE SOURCELIST CREATION OCCURS HERE!")
+
+# ----------------------------------------------------------------------------------------------------------------------
