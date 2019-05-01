7213858f847f9a11558613f4e4875d4a2fd9d3f6
Michael Dulude <dulude@stsci.edu>
2019-05-01 11:18:11 -0400

    drizzlepac/hlautils/run_sourcelist_generation.py: replaced hard-coded obs_info_dict declaration with pickle file input

--- drizzlepac/hlautils/run_sourcelist_generation.py
+++ drizzlepac/hlautils/run_sourcelist_generation.py
@@ -3,28 +3,13 @@ acs_10265_01 values. This will speed development of sourcelist_generation.py bec
 without having to run runhlaprocessing.py first.
 
 """
-
-import collections
+import pickle
+import sys
 
 import sourcelist_generation
 
-obs_info_dict = collections.OrderedDict(
-    {'filter product 00':
-      {'info': '10265 01S ACS WFC F606W',
-       'files': ['j92c01b4q_flc.fits', 'j92c01b5q_flc.fits', 'j92c01b7q_flc.fits', 'j92c01b9q_flc.fits'],
-       'product filenames': {'image': 'hst_10265_01S_ACS_WFC_F606W.fits',
-                             'source catalog': 'hst_10265_01S_ACS_WFC_F606W.cat'},
-       'subproduct #0 filenames': {'image': 'hst_10265_01S_ACS_WFC_F606W_j92c01b4q_00.fits',
-                                   'source catalog': 'hst_10265_01S_ACS_WFC_F606W_j92c01b4q_00.cat'},
-       'subproduct #1 filenames': {'image': 'hst_10265_01S_ACS_WFC_F606W_j92c01b5q_01.fits',
-                                   'source catalog': 'hst_10265_01S_ACS_WFC_F606W_j92c01b5q_01.cat'},
-       'subproduct #2 filenames': {'image': 'hst_10265_01S_ACS_WFC_F606W_j92c01b7q_02.fits',
-                                   'source catalog': 'hst_10265_01S_ACS_WFC_F606W_j92c01b7q_02.cat'},
-       'subproduct #3 filenames': {'image': 'hst_10265_01S_ACS_WFC_F606W_j92c01b9q_03.fits',
-                                   'source catalog': 'hst_10265_01S_ACS_WFC_F606W_j92c01b9q_03.cat'}},
-     'total detection product 00':
-      {'info': '10265 01S ACS WFC',
-       'files': ['j92c01b4q_flc.fits', 'j92c01b5q_flc.fits', 'j92c01b7q_flc.fits', 'j92c01b9q_flc.fits'],
-       'product filenames': {'image': 'hst_10265_01S_ACS_WFC.fits',
-                             'source catalog': 'hst_10265_01S_ACS_WFC.cat'}}})
+pickle_in = open(sys.argv[1], "rb")
+obs_info_dict = pickle.load(pickle_in)
+pickle_in.close()
+
 sourcelist_generation.create_sourcelists(obs_info_dict)
