8f076226f169424ef74ed2de973e46509b9ef8c1
Warren Hack <hack@stsci.edu>
2019-07-16 00:32:36 -0400

    Working version of get_sky_cells

--- drizzlepac/hlautils/cell_utils.py
+++ drizzlepac/hlautils/cell_utils.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 
+from matplotlib import pyplot as plt
 from scipy import ndimage
 from scipy.ndimage import morphology
 import numpy as np
@@ -20,10 +21,15 @@ from .. import wcs_functions
 _fpath = os.path.abspath(os.path.dirname(__file__))
 PCELL_PATH = os.path.join(os.path.dirname(_fpath), 'pars')
 PCELL_FILENAME = 'allsky_cells.fits'
+PCELL_STRLEN = 4
 
-SKYCELL_NAME_FMT = "skycell_{:04d}_x{:03d}y{:03d}"
+# SkyCell format: "skycell_p0000_x000y000"
+SKYCELL_NAME_FMT = f"skycell_p{{:{str(PCELL_STRLEN).zfill(2)}d}}_x{{:03d}}y{{:03d}}"
 SKYCELL_NXY = 50
 SKYCELL_OVERLAP = 256
+
+NDIMAGE_STRUCT2 = ndimage.generate_binary_structure(2, 2)
+
 
 def get_sky_cells(visit_input, input_path=None, scale=None, cell_size=None):
     """Return all sky cells that overlap the exposures in the input.
@@ -84,17 +90,17 @@ def get_sky_cells(visit_input, input_path=None, scale=None, cell_size=None):
 
     # Initialize all sky tessellation object definitions
     # This includes setting the pixel scale.
-    sky_grid = AllSky(scale=scale, cell_size=cell_size)
+    sky_grid = GridDefs(scale=scale, cell_size=cell_size)
 
     # build reference wcs for combined footprint of all input exposures
     meta_wcs = wcs_functions.make_mosaic_wcs(expnames, scale=sky_grid.scale)
 
     # create footprint on the sky (as a tangent plane array) for all input exposures using meta_wcs
-    footprint = SkyFootprint(meta_wcs).build(expnames)
-    footprint_hdu = footprint.get_footprint_hdu()
+    footprint = SkyFootprint(meta_wcs)
+    footprint.build(expnames)
 
     # Use this footprint to identify overlapping sky cells
-    sky_cells = sky_grid.find_sky_cells(footprint_hdu)
+    sky_cells = sky_grid.get_sky_cells(footprint)
 
     return sky_cells
 
@@ -103,7 +109,6 @@ class SkyFootprint(object):
 
     def __init__(self, meta_wcs):
 
-        self.struct2 = ndimage.generate_binary_structure(2, 2)
         self.meta_wcs = meta_wcs
 
         self.total_mask = np.zeros(meta_wcs.array_shape, dtype=np.int16)
@@ -121,48 +126,54 @@ class SkyFootprint(object):
             sci_extns = wcs_functions.get_extns(exp)
             for sci in sci_extns:
                 wcs = HSTWCS(exp, ext=sci)
-                edges = []
-                edges += [[0, i] for i in range(wcs.naxis2)]
-                edges += [[wcs.naxis1, i] for i in range(wcs.naxis2)]
-                edges += [[i, 0] for i in range(wcs.naxis1)]
-                edges += [[i, wcs.naxis2] for i in range(wcs.naxis1)]
+                edges_x = [0]*wcs.naxis2 + [wcs.naxis1-1]*wcs.naxis2 + list(range(wcs.naxis1)) * 2
+                edges_y = list(range(wcs.naxis2)) * 2 + [0]*wcs.naxis1 + [wcs.naxis2-1]*wcs.naxis1 
 
-                sky_edges = wcs.pixel_to_world_values(edges)
+                sky_edges = wcs.pixel_to_world_values(np.vstack([edges_x, edges_y]).T)
                 meta_edges = self.meta_wcs.world_to_pixel_values(sky_edges).astype(np.int32)
-                blank[meta_edges[:, 0], meta_edges[:, 1]] = 1
+                blank[meta_edges[:, 1], meta_edges[:, 0]] = 1
 
                 # Fill in outline of each chip
-                blank = morphology.binary_dilation(blank, structure=self.struct2)
+                blank = morphology.binary_dilation(blank, structure=NDIMAGE_STRUCT2)
                 blank = morphology.binary_fill_holes(blank)
-                blank = morphology.binary_erosion(blank, structure=self.struct2)
+                blank = morphology.binary_erosion(blank, structure=NDIMAGE_STRUCT2)
 
             self.total_mask += blank.astype(np.int16)
 
     # Methods with 'find' compute values
     # Methods with 'get' return values
     def find_footprint(self):
+        if self.total_mask is None:
+            print("Please add exposures before computing footprint...")
         self.footprint = np.clip(self.total_mask, 0, 1)
 
     def find_edges(self):
-        if not self.footprint:
+        if self.footprint is None:
             self.find_footprint()
         edges = morphology.binary_erosion(self.footprint).astype(np.int16)
         self.edges = self.footprint - edges
 
+    def find_corners(self):
+        if self.footprint is None:
+            self.find_footprint()
+                
     def get_edges_sky(self):
-        if not self.edges:
+        if self.edges is None:
             self.find_edges()
         edges = np.where(self.edges)
         self.edges_ra, self.edges_dec = self.meta_wcs.pixel_to_world_values(edges[0], edges[1])
         # close the polygon
-        self.edges.ra = np.append(self.edges_ra, [self.edges_ra[0]], axis=0)
+        self.edges_ra = np.append(self.edges_ra, [self.edges_ra[0]], axis=0)
         self.edges_dec = np.append(self.edges_dec, [self.edges_dec[0]], axis=0)
         return self.edges_ra, self.edges_dec
 
     def build_polygon(self):
-        if not self.edges_ra:
+        if self.edges_ra is None:
             self.get_edges_sky()
-        self.polygon = SphericalPolygon.from_radec(self.edges_ra, self.edges_dec, self.meta_wcs.wcs.crval)
+
+        self.polygon = SphericalPolygon.from_radec(self.edges_ra,
+                                                   self.edges_dec,
+                                                   self.meta_wcs.wcs.crval)
 
     def _get_fits_hdu(self, data, filename=None, overwrite=True):
         hdulist = fits.HDUList()
@@ -173,12 +184,12 @@ class SkyFootprint(object):
         return hdulist
 
     def get_footprint_hdu(self, filename=None, overwrite=True):
-        if not self.footprint:
+        if self.footprint is None:
             self.find_footprint()
         return self._get_fits_hdu(self.footprint, filename=filename, overwrite=overwrite)
 
     def get_edges_hdu(self, filename=None, overwrite=True):
-        if not self.edges:
+        if self.edges is None:
             self.find_edges()
         return self._get_fits_hdu(self.edges, filename=filename, overwrite=overwrite)
 
@@ -264,7 +275,12 @@ class ProjectionCell(object):
 
     def __init__(self, index=None, band=None, scale=None,
                         nxy=None, overlap=None):
-        """Build projection cell for cell with name `skycell_NNNNN`"""
+        """Build projection cell for cell with name `skycell_NNNNN`
+
+        If `band` is not specified, it will open the grid definitions file to
+        obtain the band definition for the cell with the specified `index`.
+
+        """
         if band:
             self.band_index = index
             self.band = band
@@ -272,7 +288,6 @@ class ProjectionCell(object):
             self.scale = scale
         else:
             self._from_index(index)
-
 
         # Record user-specified sky cell defaults
         if overlap:
@@ -286,7 +301,7 @@ class ProjectionCell(object):
     def _from_index(self, id):
         grid_defs = GridDefs()
         self.band = grid_defs.find_ring_by_id(id)
-        self.band_index = id - self.band['projcell'] + 1
+        self.band_index = id - self.band['projcell']
         self.cell_id = id
         self.scale = grid_defs.scale
         self.sc_overlap = grid_defs.sc_overlap
@@ -310,35 +325,92 @@ class ProjectionCell(object):
         # apply new definition to cell WCS
         self.wcs.wcs.crpix = [naxis1 / 2. + 0.5, naxis2 / 2. + 0.5]
         self.wcs.pixel_shape = (naxis1, naxis2)
+        self.corners = self.wcs.calc_footprint()
+        # close the polygon
+        self.corners = np.append(self.corners, [self.corners[0]], axis=0)
+
+    def build_mask(self):
+        naxis1, naxis2 = self.wcs.pixel_shape
+        edges_x = [0]*naxis2 + [naxis1-1]*naxis2 + list(range(naxis1)) * 2
+        edges_y = list(range(naxis2)) * 2 + [0]*naxis1 + [naxis2-1]*naxis1 
+        
+        mask = np.zeros(self.wcs.array_shape, dtype=np.int16)
+        mask[edges_y, edges_x] = 1
+
+        # Fill in outline of each chip
+        mask = morphology.binary_dilation(mask, structure=NDIMAGE_STRUCT2)
+        mask = morphology.binary_fill_holes(mask)
+        mask = morphology.binary_erosion(mask, structure=NDIMAGE_STRUCT2)
+        self.mask = mask
 
     def build_polygon(self):
-        corners = self.wcs.calc_footprint()
-        # close the polygon
-        corners = np.append(corners, [corners[0]], axis=0)
+
         inner_pix = self.wcs.pixel_to_world_values(2, 2)
         # define polygon on the sky
-        self.polygon = SphericalPolygon.from_radec(corners[:, 0], corners[:, 1], inner_pix)
+        self.polygon = SphericalPolygon.from_radec(self.corners[:, 0], self.corners[:, 1], inner_pix)
 
-    def find_sky_cells(self, skyfootprint, nxy=None, overlap=None):
+
+    def find_sky_cells(self, mosaic, nxy=None, overlap=None):
         """Return the sky cell indices from this projection cell that overlap the input footprint"""
         # Record values for sky cell definitions provided by user, if any.
         if nxy:
             self.sc_nxy = nxy
         if overlap:
             self.sc_overlap = overlap
+        print("Looking for sky cells in PROJECTION_CELL {}".format(self.cell_id))
 
+        skycell00 = SkyCell(x=0, y=0, projection_cell=self)
         skycells = {}
-        # Get the polygon for the input mosaic
-        skypoly = skyfootprint.build_polygon()
-        # for each sky cell, build the polygon and look for overlap with input mosaic poly
-        for xi in range(1, nxy + 1):
-            for yi in range(1, nxy + 1):
-                skycell = SkyCell(xi, yi, self, nxy, overlap)
-                skycell.build_polygon()
-                if skypoly.overlap(skycell.polygon) > 0.0:
+
+        # Get the edges of the mosaic on the sky
+        mosaic_ra, mosaic_dec = mosaic.get_edges_sky()
+        # Convert edges to positions in projection cell 
+        mosaic_edges = self.wcs.world_to_pixel_values(mosaic_ra, mosaic_dec)
+        # Determine roughly what sky cells overlap this mosaic
+        mosaic_edges[0] = (mosaic_edges[0] / skycell00.wcs.pixel_shape[0] + 0.5).astype(np.int32)
+        mosaic_edges[1] = (mosaic_edges[1] / skycell00.wcs.pixel_shape[1] + 0.5).astype(np.int32)
+        mosaic_xr = [mosaic_edges[0].min() - 1, mosaic_edges[0].max() + 1]
+        mosaic_yr = [mosaic_edges[1].min() - 1, mosaic_edges[1].max() + 1]
+
+        print("SkyCell Ranges: {}, {}".format(mosaic_xr, mosaic_yr))
+        # for each suspected sky cell or neighbor, look for any pixel by pixel 
+        #    overlap with input mosaic footprint
+        for xi in range(mosaic_xr[0], mosaic_xr[1]):
+            for yi in range(mosaic_yr[0], mosaic_yr[1]):
+                skycell = SkyCell(x=xi, y=yi, projection_cell=self)
+                skycell.build_mask()
+
+                # Translate mosaic edges into SkyCell WCS coordinate frame
+                mosaic_xy = skycell.wcs.world_to_pixel_values(mosaic_ra, mosaic_dec)
+                
+                # Identify edge pixels which fall outside the sky cell
+                #  by comparing to each direction (-X, +X, -Y, +Y) separately
+                mosaic_offcell = mosaic_xy[0] < 0
+                mosaic_offcell = np.bitwise_or(mosaic_offcell, 
+                                                mosaic_xy[0] > skycell.wcs.pixel_shape[0])
+                mosaic_offcell = np.bitwise_or(mosaic_offcell, mosaic_xy[1] < 0) 
+                mosaic_offcell = np.bitwise_or(mosaic_offcell, 
+                                                mosaic_xy[1] > skycell.wcs.pixel_shape[1])
+
+                # With all out of bounds pixels masked out, see if any are left
+                sc_overlap = np.any(~mosaic_offcell)
+
+                print("    Checking SkyCell {},{} for overlap: {}".format(xi, yi, sc_overlap))
+                if sc_overlap:
+                    # We found overlapping pixels from mosaic, so return this SkyCell
+                    print("   Found overlap in SkyCell {}".format(skycell.sky_cell_id))
                     skycells[skycell.sky_cell_id] = skycell
 
         return skycells
+
+    def plot(self, output=None, color='b'):
+        fig = plt.figure(figsize=(8, 6))
+        ax = fig.add_subplot(111, projection="mollweide")
+        ax.plot(self.corners[:, 0], self.corners[:, 1], color)
+        if output:
+            fig.write(output)
+        return ax
+
 
 class SkyCell(object):
 
@@ -351,8 +423,7 @@ class SkyCell(object):
         else:
             self.x_index = x
             self.y_index = y
-            self.sky_cell_id = SKYCELL_NAME_FMT.format(projection_cell.cell_id,
-                                                          str(x).zfill(3), str(y).zfill(3))
+            self.sky_cell_id = SKYCELL_NAME_FMT.format(projection_cell.cell_id, x, y)
             self.projection_cell = projection_cell
 
         self.overlap = self.projection_cell.sc_overlap  # overlap between sky cells
@@ -363,7 +434,7 @@ class SkyCell(object):
     def _from_name(self, name):
         # parse name into projection cell and sky cell designations
         sc_names = name.split('_')
-        pcell_id = int(sc_names[1])
+        pcell_id = int(sc_names[1][1:])
 
         self.x_index = int(sc_names[2][1:4])
         self.y_index = int(sc_names[2][5:8])
@@ -371,7 +442,7 @@ class SkyCell(object):
         self.sky_cell_id = name
 
     def __repr__(self):
-        return self.sky_cell_id
+        return "SkyCell object: {}".format(self.sky_cell_id)
 
     def rescale(self, scale):
         """Return WCS which has a user-defined scale."""
@@ -382,8 +453,8 @@ class SkyCell(object):
         pc_ny = self.projection_cell.wcs.pixel_shape[1]
         naxis1 = int((pc_nx - 2 * self.overlap) / self.nxy + 0.5)
         naxis2 = int((pc_ny - 2 * self.overlap) / self.nxy + 0.5)
-        crpix1 = self.projection_cell.wcs.wcs.crpix[0] - (((self.x_index - 1) * naxis1) - self.overlap)
-        crpix2 = self.projection_cell.wcs.wcs.crpix[1] - (((self.y_index - 1) * naxis2) - self.overlap)
+        crpix1 = self.projection_cell.wcs.wcs.crpix[0] - (((self.x_index) * naxis1) - self.overlap)
+        crpix2 = self.projection_cell.wcs.wcs.crpix[1] - (((self.y_index) * naxis2) - self.overlap)
 
         # apply definitions
         self.wcs = astropy.wcs.WCS(naxis=2)
@@ -395,13 +466,29 @@ class SkyCell(object):
         self.wcs.ltv1 = self.projection_cell.wcs.wcs.crpix[0] - crpix1
         self.wcs.ltv2 = self.projection_cell.wcs.wcs.crpix[1] - crpix2
 
-    def build_polygon(self):
-        corners = self.wcs.calc_footprint()
+        self.corners = self.wcs.calc_footprint()
         # close the polygon
-        corners = np.append(corners, [corners[0]], axis=0)
+        self.corners = np.append(self.corners, [self.corners[0]], axis=0)
+
+    def build_polygon(self):
         inner_pix = self.wcs.pixel_to_world_values(2, 2)
         # define polygon on the sky
-        self.polygon = SphericalPolygon.from_radec(corners[:, 0], corners[:, 1], inner_pix)
+        self.polygon = SphericalPolygon.from_radec(self.corners[:, 0],
+                                                   self.corners[:, 1],
+                                                   inner_pix)
+    def build_mask(self):
+        naxis1, naxis2 = self.wcs.pixel_shape
+        edges_x = [0]*naxis2 + [naxis1-1]*naxis2 + list(range(naxis1)) * 2
+        edges_y = list(range(naxis2)) * 2 + [0]*naxis1 + [naxis2-1]*naxis1 
+
+        mask = np.zeros(self.wcs.array_shape, dtype=np.int16)
+        mask[edges_y, edges_x] = 1
+
+        # Fill in outline of each chip
+        mask = morphology.binary_dilation(mask, structure=NDIMAGE_STRUCT2)
+        mask = morphology.binary_fill_holes(mask)
+        mask = morphology.binary_erosion(mask, structure=NDIMAGE_STRUCT2)
+        self.mask = mask
 
 #
 # Utility functions used in generating or supporting the grid definitions
@@ -414,7 +501,7 @@ def update_grid_defs(pc_size=5.0, output=None, grid_file=None):
     pc_size : float
         Size of each side of the projection cell or width of each band on
         the sky in degrees.  If `None`, the default value will be read in
-        from the `PCSIZE` keyword from the PRIMARY header of the default
+        from the `PC_SIZE` keyword from the PRIMARY header of the default
         grid definitions file.
 
     output : str, optional
@@ -428,10 +515,10 @@ def update_grid_defs(pc_size=5.0, output=None, grid_file=None):
         grid_file = os.path.join(PCELL_PATH, PCELL_FILENAME)
     grid_defs = fits.open(grid_file)
     grid = grid_defs[1].data
-    pc_scale = grid_defs[0].header['PCSCALE']
+    pc_scale = grid_defs[0].header['PC_SCALE']
 
     if not pc_size:
-        pc_size = grid_defs[0].header['PCSIZE']
+        pc_size = grid_defs[0].header['PC_SIZE']
 
     pos_angle = [0.0 * u.deg, 90.0 * u.deg, 180.0 * u.deg, 270.0 * u.deg]
     pc_edge = pc_size / 2.0 * u.deg
