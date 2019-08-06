85c2214778c9ebd94543a4d08fd4d28b50231059
Michael Dulude <dulude@stsci.edu>
2019-08-06 15:30:14 -0400

    minor tweaks

--- drizzlepac/test_config_utils.py
+++ drizzlepac/test_config_utils.py
@@ -12,12 +12,13 @@ input_filename = sys.argv[1]
 
 obs_info_dict, expo_list, filt_list, total_list = poller_utils.interpret_obset_input('j92c01.out')
 
-param_filename = "superparamfile_1.json"
+param_filename = "superparamfile.json"
 
 for item in expo_list+filt_list+total_list:
-    item.pars = config_utils.HapConfig(item,output_custom_pars_file=param_filename,use_defaults=False)
+    item.pars = config_utils.HapConfig(item,input_custom_pars_file=param_filename,use_defaults=False)
 #
-# print(expo_list[0].pars.get_pars("alignment"))
+print(" ")
+print(expo_list[0].pars.get_pars("alignment"))
 #
 # pdb.set_trace()
 
