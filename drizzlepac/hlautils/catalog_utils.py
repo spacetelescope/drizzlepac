537e3c69c86f36eb0e81538a52232b7018d3aa39
Michael Dulude <dulude@stsci.edu>
2019-08-14 14:13:04 -0400

    drizzlepac/hlautils/catalog_utils.py: Minor cosmetic tweaks

--- drizzlepac/hlautils/catalog_utils.py
+++ drizzlepac/hlautils/catalog_utils.py
@@ -686,7 +686,6 @@ class HAPPointCatalog(HAPCatalogBase):
         skyannulus_pix = skyannulus_arcsec/platescale
         dskyannulus_arcsec = 0.25
         dskyannulus_pix = dskyannulus_arcsec/platescale
-        # gain = 5060.0
         salgorithm = 'mode'
         # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         # Compute AB mag zeropoint.
@@ -694,7 +693,7 @@ class HAPPointCatalog(HAPCatalogBase):
         photflam = self.image.imghdu[1].header['photflam']
         ab_zeropoint = -2.5 * np.log10(photflam) - 21.10 - 5.0 * np.log10(photplam) + 18.6921
 
-        #compute average average gain
+        # Compute average gain
         gain = self.image.imghdu[0].header['exptime'] * np.mean([self.image.imghdu[0].header['atodgna'],self.image.imghdu[0].header['atodgnb'],self.image.imghdu[0].header['atodgnc'],self.image.imghdu[0].header['atodgnd']])
 
         # load in coords of sources identified in total product
