eded10be46819a4a696db932987c95af62a16110
Warren Hack <hack@stsci.edu>
2019-08-19 10:52:24 -0400

    Debug retrieve_observation for Windows

--- drizzlepac/hlautils/astroquery_utils.py
+++ drizzlepac/hlautils/astroquery_utils.py
@@ -112,7 +112,7 @@ def retrieve_observation(obsid, suffix=['FLC'], archive=False, clobber=False):
             # Identify what sub-directory was created by astroquery for the
             # download
             if download_dir is None:
-                new_file_path = os.path.dirname(file)
+                new_file_path = os.path.dirname(os.path.abspath(file))
                 file_path = file.split(os.sep)
                 log.info("Defining download directory based on: {} or [new]{}".format(file_path, new_file_path))
                 if '.' in file_path:
