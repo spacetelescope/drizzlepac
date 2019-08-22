e5b0782f1156408e44bed56ea88fbda30a824510
Michael Dulude <dulude@stsci.edu>
2019-08-22 16:40:37 -0400

    drizzlepac/hapsequencer.py: copied subroutine hapcatalog.run_catalog_utils() into hapsequencer.py subroutine 'create_catalog_products'. Modified existing calling code in run_hla_processing() to execute it.

--- drizzlepac/hapsequencer.py
+++ drizzlepac/hapsequencer.py
@@ -9,6 +9,7 @@
 import datetime
 import glob
 import os
+import pdb # TODO: remove before final deployment
 import pickle
 import sys
 import traceback
@@ -207,6 +208,87 @@ param_dict = {
             "scale_factor_list": [2.3e-6, 4.e-6, 8.e-6, 2.e-5, 0.0005, 0.005, 0.005, 0.015, 0.45, 1.],
             # "scale_factor_list_orig": [2.3e-6, 4.e-6, 8.e-6, 2.e-5, 6.e-5, 0.0005, 0.005, 0.015, 0.45, 1.],
             "proximity_binary": "yes"}}}
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+def create_catalog_products(total_list, debug=False, phot_mode='both'):
+    """This subroutine utilizes hlautils/catalog_utils module to produce photometric sourcelists for the specified
+    total drizzle product and it's associated child filter products.
+
+    Parameters
+    ----------
+    total_list : drizzlepac.hlautils.Product.TotalProduct
+        total drizzle product that will be processed by catalog_utils. catalog_utils will also create photometric
+        sourcelists for the child filter products of this total product.
+
+    debug : bool, optional
+        generate ds9 region file counterparts to the photometric sourcelists? Default value is False.
+
+    phot_mode : str, optional
+        Which algorithm should be used to generate the sourcelists? 'aperture' for aperture photometry;
+        'segment' for segment map photometry; 'both' for both 'segment' and 'aperture'. Default value is 'both'.
+
+    Returns
+    -------
+    product_list : list
+        list of all catalogs generated.
+    """
+    product_list = []
+    for total_product_obj in total_list:
+        # determine total product filename
+        if os.path.exists(total_product_obj.product_basename+"_drc.fits"):
+            total_product_name = total_product_obj.product_basename + "_drc.fits"
+        else:
+            total_product_name = total_product_obj.product_basename + "_drz.fits"
+
+        # Instantiate filter catalog product object
+        total_product_catalogs = HAPCatalogs(total_product_name, types=phot_mode, debug=debug)
+
+        # Identify sources to be measured by filter photometry step
+        total_product_catalogs.identify()
+
+        #write out list(s) of identified sources
+        total_product_catalogs.write()
+
+        #append total product catalogs to list
+        if phot_mode in ['aperture', 'both']:
+            product_list.append(total_product_obj.point_cat_filename)
+        if phot_mode in ['segment', 'both']:
+            product_list.append(total_product_obj.segment_cat_filename)
+
+        # build dictionary of total_product_catalogs.catalogs[*].sources to use for
+        # filter photometric catalog generation
+        sources_dict = {}
+        for cat_type in total_product_catalogs.catalogs.keys():
+            sources_dict[cat_type] = {}
+            sources_dict[cat_type]['sources'] = total_product_catalogs.catalogs[cat_type].sources
+            if cat_type == "segment":
+                sources_dict['segment']['kernel'] = total_product_catalogs.catalogs['segment'].kernel
+
+        for filter_product_obj in total_product_obj.fdp_list:
+            # determine filter product filename
+            if os.path.exists(filter_product_obj.product_basename + "_drc.fits"):
+                filter_product_name = filter_product_obj.product_basename + "_drc.fits"
+            else:
+                filter_product_name = filter_product_obj.product_basename + "_drz.fits"
+
+            # Instantiate filter catalog product object
+            filter_product_catalogs = HAPCatalogs(filter_product_name, types=phot_mode,
+                                                  debug=debug, tp_sources=sources_dict)
+            # Perform photometry
+            filter_product_catalogs.measure()
+
+            # Write out photometric catalog(s)
+            filter_product_catalogs.write()
+
+            # append filter product catalogs to list
+            if phot_mode in ['aperture', 'both']:
+                product_list.append(filter_product_obj.point_cat_filename)
+            if phot_mode in ['segment', 'both']:
+                product_list.append(filter_product_obj.segment_cat_filename)
+
+    return product_list
+
 # ----------------------------------------------------------------------------------------------------------------------
 
 def create_drizzle_products(obs_info_dict, total_list, meta_wcs):
@@ -314,7 +396,7 @@ def create_drizzle_products(obs_info_dict, total_list, meta_wcs):
 # ----------------------------------------------------------------------------------------------------------------------
 
 
-def run_hla_processing(input_filename, result=None, debug=False):
+def run_hla_processing(input_filename, result=None, debug=False, phot_mode='aperture'):
     # This routine needs to return an exit code, return_value, for use by the calling
     # Condor/OWL workflow code: 0 (zero) for success, 1 for error condition
     return_value = 0
@@ -390,24 +472,15 @@ def run_hla_processing(input_filename, result=None, debug=False):
         driz_list = create_drizzle_products(obs_info_dict, total_list, meta_wcs)
         product_list += driz_list
 
-        """
-        # 7: Create source catalogs from newly defined products (HLA-204)
-        log.info("7: (WIP) Create source catalog from newly defined product")
-        if debug:
-            pickle_filename = input_filename.replace(".out", ".pickle")
-            if os.path.exists(pickle_filename):
-                os.remove(pickle_filename)
-            pickle_out = open(pickle_filename, "wb")
-            pickle.dump([obs_info_dict, param_dict], pickle_out)
-            pickle_out.close()
-            print("Wrote obs_info_dict to pickle file {}".format(pickle_filename))
 
+        # Create source catalogs from newly defined products (HLA-204)
+        log.info("Create source catalog from newly defined product")
         if 'total detection product 00' in obs_info_dict.keys():
-            catalog_list = sourcelist_generation.create_sourcelists(obs_info_dict, param_dict)
+            catalog_list = create_drizzle_products(total_list, debug=debug, phot_mode=phot_mode)
             product_list += catalog_list
         else:
             print("Sourcelist generation step skipped.")
-
+        """
         # 8: (OPTIONAL) Determine whether there are any problems with alignment or photometry of product
         log.info("8: (TODO) (OPTIONAL) Determine whether there are any problems with alignment or photometry"
                  "of product")
