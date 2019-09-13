54cb712ab99f6efe9fe6b4dcb6970983e27d64a6
Michael Dulude <dulude@stsci.edu>
2019-09-13 08:48:42 -0400

    drizzlepac/test_config_utils.py: minor refactoring to keep things up to date

--- drizzlepac/test_config_utils.py
+++ drizzlepac/test_config_utils.py
@@ -12,19 +12,26 @@ from drizzlepac.hlautils import poller_utils
 
 input_filename = sys.argv[1]
 
-# obs_info_dict, expo_list, filt_list, total_list = poller_utils.interpret_obset_input('j92c01.out')
-#
-# param_filename = "superparamfile.json"
-#
-# for item in expo_list+filt_list+total_list:
-#     item.configobj_pars = config_utils.HapConfig(item,input_custom_pars_file=param_filename,use_defaults=False)
+obs_info_dict, total_list = poller_utils.interpret_obset_input(input_filename)
+out_pars_file = None
+for total_item in total_list:
+
+    total_item.configobj_pars = config_utils.HapConfig(total_item, output_custom_pars_file=out_pars_file,
+                                                       use_defaults=True)
+    for filter_item in total_item.fdp_list:
+        filter_item.configobj_pars = config_utils.HapConfig(filter_item, output_custom_pars_file=out_pars_file,
+                                                            use_defaults=True)
+    for expo_item in total_item.edp_list:
+        expo_item.configobj_pars = config_utils.HapConfig(expo_item, output_custom_pars_file=out_pars_file,
+                                                          use_defaults=True)
+
 # #
 # print(" ")
 # print(expo_list[0].configobj_pars.get_pars("alignment"))
 #
 
-rv = hapsequencer.run_hap_processing(input_filename)
-# pdb.set_trace()
+# rv = hapsequencer.run_hap_processing(input_filename)
+pdb.set_trace()
 
 # config_utils.reformat_json_file("superparamfile.json","new_superparamfile.json")
 
