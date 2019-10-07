641b48d86935f30ea91253890dd19665b30eca4b
Michael Dulude <dulude@stsci.edu>
2019-10-07 14:14:09 -0400

    drizzlepac/devutils/make_mask_file.py: refactored make_mask_file_old().

--- drizzlepac/devutils/make_mask_file.py
+++ drizzlepac/devutils/make_mask_file.py
@@ -5,18 +5,18 @@ import pdb
 
 def make_mask_file_old(in_imgname):
     from scipy import ndimage
-    maskfilename = in_imgname.replace("_drz.fits","_msk.fits")
-    in_hdulist = fits.open(in_imgname)
-    out_hdulist = fits.PrimaryHDU(in_hdulist[2].data)
+    out_imgname = in_imgname.replace("wht","msk")
+
+    in_wht_hdulist = fits.open(in_imgname)
+    in_sat_hdulist = fits.open(in_imgname.replace("wht","sat"))
+
+    out_hdulist = in_wht_hdulist.copy()
+    sat_data = in_sat_hdulist[0].data
+    ndimage.binary_dilation(sat_data,iterations=11)
+    out_hdulist[0].data = np.where(in_wht_hdulist[0].data+sat_data == 0, 1, 0)
 
 
-
-    out_hdulist.data = np.where(out_hdulist.data == 0, 1, 0)
-    ndimage.binary_fill_holes(out_hdulist.data)
-
-
-    out_hdulist.writeto(maskfilename)
-    # create frame shaped like in_image filled with 1s
+    out_hdulist.writeto(out_imgname)
 
 def make_mask_file(drz_image,input_list):
     from drizzlepac.hlautils import cell_utils as cutils
@@ -33,8 +33,7 @@ def make_mask_file(drz_image,input_list):
     print("Wrote {}".format(maskfilename))
 
 if __name__ == "__main__":
-    in_imgname = "hst_10265_01_acs_wfc_f606w_j92c01_drc.fits"
-    in_imgname = "hst_10265_01_acs_wfc_total_drz.fits"
-    input_list = ["j92c01b4q_flc.fits", "j92c01b5q_flc.fits", "j92c01b7q_flc.fits", "j92c01b9q_flc.fits"]
-    make_mask_file(in_imgname,input_list)
-    # make_mask_file_old("hst_10265_01_acs_wfc_total_drz.fits")
+    # in_imgname = "hst_10265_01_acs_wfc_f606w_j92c01_drc.fits"
+    # input_list = ["j92c01b4q_flc.fits", "j92c01b5q_flc.fits", "j92c01b7q_flc.fits", "j92c01b9q_flc.fits"]
+    # make_mask_file(in_imgname,input_list)
+    make_mask_file_old("hst_10265_01_acs_wfc_f606w_wht.fits")
