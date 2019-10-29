203774071c021fe8d594a02e07fec7df34f07a26
Michael Dulude <dulude@stsci.edu>
2019-10-29 10:39:27 -0400

    drizzlepac/devutils/hap_pars_attendance_checker.py: development done.

--- drizzlepac/devutils/hap_pars_attendance_checker.py
+++ drizzlepac/devutils/hap_pars_attendance_checker.py
@@ -7,9 +7,57 @@ import pdb
 
 code_dir = os.path.abspath(__file__)
 base_dir = os.path.dirname(os.path.dirname(code_dir))
-full_pars_path = os.path.join(base_dir, "pars/hap_pars")
-json_list = Path(full_pars_path).glob("**/*.json")
-pdb.set_trace()
+base_pars_path = os.path.join(base_dir, "pars/hap_pars")
+full_json_list = Path(base_pars_path).glob("**/*.json")
 
-param_list = ['default_parameters', 'user_parameters']
+param_dict = {'default_parameters': [], 'user_parameters':[]}
 inst_det_list = ['acs_hrc', 'acs_sbc', 'acs_wfc', 'wfc3_ir', 'wfc3_uvis']
+step_list = ['alignment', 'astrodrizzle', 'catalog_generation', 'quality_control']
+
+print("DEFAULT USER     FILE")
+# check for the case where all files are missing for given instrument/detector/step
+for inst_det in inst_det_list:
+    inst = inst_det.split("_")[0]
+    det = inst_det.split("_")[1]
+    for step in step_list:
+        status = ""
+        for param_set in param_dict.keys():
+            search_string = "{}/{}/{}/{}/{}_{}*.json".format(base_pars_path, param_set, inst,det, inst_det, step)
+            files_found = glob.glob(search_string)
+
+            if param_set == "default_parameters":
+                d_status = "OK     "
+                if not files_found:
+                    d_status = "MISSING"
+                status="{}{}".format(status,d_status)
+            if param_set == "user_parameters":
+                u_status = "OK     "
+                if not files_found:
+                    u_status = "MISSING"
+                short_search_string = "{}/{}/{}_{}*.json".format(inst, det, inst_det, step)
+                status = "{} {}  {}".format(status, d_status, short_search_string)
+        if d_status == "MISSING" or u_status =="MISSING":
+            print(status)
+
+# Check that all json files in default_parameters/ are also in user_parameters
+for json_file in full_json_list:
+    json_file = str(json_file)
+    for param_file_type in param_dict.keys():
+        json_path = "{}/{}/".format(base_pars_path, param_file_type)
+        if json_file.startswith(json_path):
+            param_dict[param_file_type].append(json_file.split(json_path)[1])
+
+unique_combo_list = list(set(param_dict["default_parameters"] + param_dict["user_parameters"]))
+
+for json_file in unique_combo_list:
+    d_status = "OK     "
+    u_status = "OK     "
+    if not json_file in param_dict["default_parameters"]:
+        d_status = "MISSING"
+    if not json_file in param_dict["user_parameters"]:
+        u_status = "MISSING"
+
+    print("{} {}  {}".format(d_status, u_status, json_file))
+
+
+
