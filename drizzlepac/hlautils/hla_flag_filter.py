f6d364fd1d835f86f559fe48960cf3a2d89e2f83
Michael Dulude <dulude@stsci.edu>
2019-11-19 13:12:49 -0500

    drizzlepac/hlautils/hla_flag_filter.py: added test_display_catalog_bit_populations.display_catalog_bit_populations() and test_display_catalog_bit_populations.deconstruct_flag().

--- drizzlepac/hlautils/hla_flag_filter.py
+++ drizzlepac/hlautils/hla_flag_filter.py
@@ -41,6 +41,7 @@ import sys
 import time
 
 from astropy.io import fits as fits
+from astropy.table import Table
 import numpy
 import scipy
 import scipy.ndimage
@@ -164,6 +165,7 @@ def run_source_list_flagging(drizzled_image, flt_list, param_dict, exptime, plat
     catalog_data = hla_nexp_flags(drizzled_image, flt_list, param_dict, plate_scale, catalog_name, catalog_data,
                                   drz_root_dir, hla_flag_msk, column_titles, diagnostic_mode)
 
+    display_catalog_bit_populations(catalog_data['Flags'])
     return catalog_data
 
 # ======================================================================================================================
@@ -1637,3 +1639,87 @@ def make_mask_array(drz_image):
     mask = (erode(dilate(bigmask, kernel1), kernel2) == 0)[padding:-padding, padding:-padding]
     mask = mask.astype(numpy.int16)
     return mask
+
+
+# ======================================================================================================================
+
+def deconstruct_flag(flagval):
+    """Breaks down an integer flag value into individual component bit values.
+
+    Parameters
+    ----------
+    flagval : int
+        Flag value to deconstruct
+
+    Returns
+    -------
+    out_idx_list : list
+        a 9-element numpy array of 0s and 1s. Each element of the array represents the presence of a particular
+        bit value (element 0 = bit 0, element 1 = bit 1, ..., element 3 = bit 4 and so on...)
+    """
+    bitlist = [1, 2, 4, 8, 16, 32, 64, 128]
+    flagval = int(flagval)
+    # out_bit_list = []
+    out_idx_list = numpy.zeros(9, dtype=int)
+    if flagval == 0:
+        # out_bit_list = [0]
+        out_idx_list[0] = 1
+    if flagval > 0:
+        idx = 1
+        for bit in bitlist:
+            if flagval & bit > 0:
+                # out_bit_list.append(bit)
+                out_idx_list[idx] = 1
+            if bit > flagval:
+                break
+            idx += 1
+    return out_idx_list
+
+
+# ======================================================================================================================
+
+def display_catalog_bit_populations(flag_data):
+    """Breaks all input flag values down into their constituent bit values and displays a bit-by-bit population summary
+
+    Parameters
+    ----------
+    flag_data : astropy.table.column.Column object
+        'Flags' column of a given sourcelist to analyze
+
+    Returns
+    -------
+    Nothing.
+    """
+    bit_list = [0, 1, 2, 4, 8, 16, 32, 64, 128]
+    flag_meanings = ['Point Source', 'Extended Source', 'Single-Pixel Saturation', 'Multi-Pixel Saturation',
+                     'Faint Magnitude Limit', 'Hot Pixel', 'Swarm Detection', 'Edge and Chip Gap',
+                     'Bleeding and Cosmic Rays']
+    flag_counts = numpy.zeros(9, dtype=int)
+    n_sources = len(flag_data)
+    for flagval in flag_data:
+        flag_counts += deconstruct_flag(flagval)
+    max_length = 5
+    for bitval in flag_counts:
+        max_length = max([max_length, len(str(bitval))])
+    print("{}".format("-"*60))
+    print("{}FLAG BREAKDOWN BY BIT".format(" "*20))
+    print("Bit   Meaning{}Count Percentage".format(" "*20))
+    fill_char = " "
+    for ctr in range(0, len(bit_list)):
+        bit_val = bit_list[ctr]
+        pct_val = 100.0*(float(flag_counts[ctr])/float(n_sources))
+        padding1 = 6 - len(str(bit_val))
+        padding2 = 27 - len(flag_meanings[ctr])
+        padding3 = max_length-len(str(flag_counts[ctr]))
+        if pct_val == 100.:
+            padding4 = 3
+        elif pct_val >= 10.:
+            padding4 = 4
+        else:
+            padding4 = 5
+        print("{}{}{}{}{}{}{}{:.3f}%".format(bit_val, fill_char*padding1, flag_meanings[ctr], padding2*fill_char,
+                                             fill_char*padding3, flag_counts[ctr], fill_char*padding4, pct_val))
+    print("\nNOTE: As the flag value for a given source can be composed ")
+    print("of multiple bits, the above percentage values need not add")
+    print("up to 100%.")
+    print("{}".format("-" * 60))
