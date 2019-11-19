0abe52322c1f26c75fae338f16928b66e8cec3ca
Michael Dulude <dulude@stsci.edu>
2019-11-19 12:25:17 -0500

    Add command-line input "log_level" to runsinglehap.py, hapsequencer.run_hap_processing (#506)
    * drizzlepac/runsinglehap.py: added new command-line input 'log_level'.
    
    * drizzlepac/hapsequencer.py: added subroutine input variable description to run_hap_processing() subroutine docstring for variable log_level.
    
    * initial commit
    
    * runsinglehap.py and hapsequencer.py now logging to the user-specified logging level.
    
    * drizzlepac/runsinglehap.py: tweaked logic tree that defines logging level so that it fails safe to logging level 'INFO'
    
    * drizzlepac/hapsequencer.py: minor tweak to log file definition
    
    * drizzlepac/runsinglehap.py: streamlined logic used to determine logging level in perform()
    
    * drizzlepac/runsinglehap.py: minor tweaks
    
    * Update drizzlepac/hapsequencer.py
    
    Co-Authored-By: mdlpstsci <mdelapena@stsci.edu>

--- drizzlepac/runsinglehap.py
+++ drizzlepac/runsinglehap.py
@@ -16,7 +16,7 @@ Python USAGE:
 """
 # Import standard Python modules
 import argparse
-
+import pdb
 import sys
 import logging
 
@@ -56,14 +56,31 @@ def perform(input_filename, **kwargs):
     debug : Boolean
         display all tracebacks, and debug information?
 
+    log_level : string
+        The desired level of verboseness in the log statements displayed on the screen and written to the .log file.
+
     Updates
     -------
     return_value : list
         a simple status value. '0' for a successful run and '1' for a failed
         run
     """
-    log.info("Starting single-visit processing of {}".format(input_filename))
+    # set up log_level as an input to hapsequencer.run_hap_processing().
+    log_level_dict = {"critical": logutil.logging.CRITICAL,
+                      "error": logutil.logging.ERROR,
+                      "warning": logutil.logging.WARNING,
+                      "info": logutil.logging.INFO,
+                      "debug": logutil.logging.DEBUG}
+    kwargs['log_level'] = kwargs['log_level'].lower()
+    if kwargs['log_level'] in log_level_dict.keys():
+        kwargs['log_level'] = log_level_dict[kwargs['log_level']]
+    else:
+        print("Log level set to default level 'log.info'.")
+        kwargs['log_level'] = logutil.logging.INFO
+
+    # execute hapsequencer.run_hap_processing()
     return_value = hapsequencer.run_hap_processing(input_filename, **kwargs)
+
     return return_value
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -76,10 +93,17 @@ def main():
     parser.add_argument('-d', '--diagnostic_mode', required=False, action='store_true', help='If this option is turned '
                         'on, additional log messages will be displayed and additional files will be created during the '
                         'course of the run.')
+    parser.add_argument('-l', '--log_level', required=False, default='info',
+                        choices=['critical', 'error', 'warning', 'info', 'debug'], help='The desired level of '
+                        'verboseness in the log statements displayed on the screen and written to the .log file. The '
+                        'level of verboseness from left to right, and includes all log statements with a log_level '
+                        'left of the specified level. Specifying "critical" will only record/display "critical" log '
+                        'statements, and specifying "error" will record/display both "error" and "critical" log '
+                        'statements, and so on.')
     user_args = parser.parse_args()
 
     print("Single-visit processing started for: {}".format(user_args.input_filename))
-    rv = perform(user_args.input_filename, diagnostic_mode=user_args.diagnostic_mode)
+    rv = perform(user_args.input_filename, diagnostic_mode=user_args.diagnostic_mode, log_level = user_args.log_level)
     print("Return Value: ", rv)
     return rv
 
