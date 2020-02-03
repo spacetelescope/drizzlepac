spacetelescope/drizzlepac

commit fc98ce67befe46a65cc8dfab4b59d055994c5d87

Improved the boolean mask creation for use with segmentation catalog, add (#575)
creation of intermediate images when in diagnostic mode, and force "preserve"
which is now an obsolete parameter to be False so no OrIg_files will be created.

drizzlepac/hlautils/catalog_utils.py
@@ -849,12 +849,18 @@ class HAPSegmentCatalog(HAPCatalogBase):
         # Check the pars to see if the "n" exposure mask has been passed.  If it has, it must be
         # converted into a boolean mask where False = actual image footprint, and True = the
         # non-illuminated portion of the images.  The True indicates to detect_sources()
-        # which pixels to ignore.
+        # which pixels to IGNORE.
         mask = pars.get('mask', None)
         if hasattr(mask, 'shape'):
+            # At this point this general mask has zeros in the non-illuminated portion, and non-zeros in
+            # the illuminated portion.  We are decreasing the footprint of the illuminated portion.
+            mask = ndimage.binary_erosion(mask, iterations=10)
+            # Now convert the mask for the purposes needed here: the non-illuminated portion is True,
+            # and the illuminated portion is False.
             mask = mask < 1
-            # outname = self.imgname.replace(".fits","_mask.fits")
-            # fits.PrimaryHDU(data=mask.astype(np.uint16)).writeto(outname)
+            if self.diagnostic_mode:
+                outname = self.imgname.replace(".fits","_mask.fits")
+                fits.PrimaryHDU(data=mask.astype(np.uint16)).writeto(outname)
 
         # If the total product sources have not been identified, then this needs to be done!
         if not self.tp_sources:
@@ -889,6 +895,9 @@ class HAPSegmentCatalog(HAPCatalogBase):
             self.segm_img = detect_sources(imgarr_bkgsub, threshold, npixels=self._size_source_box,
                                            filter_kernel=self.image.kernel,
                                            mask=mask)
+            if self.diagnostic_mode:
+                outname = self.imgname.replace(".fits","_segment.fits")
+                fits.PrimaryHDU(data = self.segm_img.data).writeto(outname)
 
             try:
                 # Deblending is a combination of multi-thresholding and watershed
@@ -898,6 +907,9 @@ class HAPSegmentCatalog(HAPCatalogBase):
                 segm_deblended_img = deblend_sources(imgarr_bkgsub, self.segm_img, npixels=self._size_source_box,
                                                      filter_kernel=self.image.kernel, nlevels=self._nlevels,
                                                      contrast=self._contrast)
+                if self.diagnostic_mode:
+                    outname = self.imgname.replace(".fits","_segment_deblended.fits")
+                    fits.PrimaryHDU(data = self.segm_deblended_img.data).writeto(outname)
 
                 # The deblending was successful, so just copy the deblended sources back to the sources attribute.
                 self.segm_img = copy.deepcopy(segm_deblended_img)
@@ -909,10 +921,6 @@ class HAPSegmentCatalog(HAPCatalogBase):
 
             # Regardless of whether or not deblending worked, this variable can be reset to None
             segm_deblended_img = None
-
-            # Clean up segments that are near or partially overlap the border of the image and relabel
-            # the segments to be sequential
-            self.segm_img.remove_border_labels(self._border, partial_overlap=True, relabel=True)
 
             # The total product catalog consists of at least the X/Y and RA/Dec coordinates for the detected
             # sources in the total drizzled image.  All the actual measurements are done on the filtered drizzled
@@ -956,14 +964,9 @@ class HAPSegmentCatalog(HAPCatalogBase):
             self.sources = self.tp_sources['segment']['sources']
             self.kernel = self.tp_sources['segment']['kernel']
 
-        # For debugging purposes only, create a segmentation image and a "regions" files to use for ds9 overlay
+        # For debugging purposes only, create a "regions" files to use for ds9 overlay of the segm_img.
         # Create the image regions file here in case there is a failure
-        if self.diagnostic_mode and self.segm_img:
-            # Generate a diagnostic_mode segmentation image
-            # indx = self.sourcelist_filename.find("-cat.ecsv")
-            # outname = self.sourcelist_filename[0:indx] + ".fits"
-            # fits.PrimaryHDU(data = self.segm_img.data).writeto(outname)
-
+        if self.diagnostic_mode:
             # Copy out only the X and Y coordinates to a "diagnostic_mode table" and cast as an Astropy Table
             # so a scalar can be added to the centroid coordinates
             tbl = self.source_cat["X-Centroid", "Y-Centroid"]

drizzlepac/hlautils/product.py
@@ -165,6 +165,9 @@ class TotalProduct(HAPProduct):
         # ...and set parameters which are computed on-the-fly
         drizzle_pars["final_refimage"] = meta_wcs
         drizzle_pars["runfile"] = self.trl_logname
+        # Setting "preserve" to false so the OrIg_files directory is deleted as the purpose
+        # of this directory is now obsolete.
+        drizzle_pars["preserve"] = False
         log.debug("The 'final_refimage' ({}) and 'runfile' ({}) configuration variables "
                   "have been updated for the drizzle step of the total drizzle product."
                   .format(meta_wcs, self.trl_logname))
@@ -308,6 +311,9 @@ class FilterProduct(HAPProduct):
         # ...and set parameters which are computed on-the-fly
         drizzle_pars["final_refimage"] = meta_wcs
         drizzle_pars["runfile"] = self.trl_logname
+        # Setting "preserve" to false so the OrIg_files directory is deleted as the purpose
+        # of this directory is now obsolete.
+        drizzle_pars["preserve"] = False
         log.debug("The 'final_refimage' ({}) and 'runfile' ({}) configuration variables "
                   "have been updated for the drizzle step of the filter drizzle product."
                   .format(meta_wcs, self.trl_logname))
@@ -379,6 +385,9 @@ class ExposureProduct(HAPProduct):
         # ...and set parameters which are computed on-the-fly
         drizzle_pars["final_refimage"] = meta_wcs
         drizzle_pars["runfile"] = self.trl_logname
+        # Setting "preserve" to false so the OrIg_files directory is deleted as the purpose
+        # of this directory is now obsolete.
+        drizzle_pars["preserve"] = False
         log.debug("The 'final_refimage' ({}) and 'runfile' ({}) configuration variables "
                   "have been updated for the drizzle step of the exposure drizzle product."
                   .format(meta_wcs, self.trl_logname))

commit 2f8e542d782a73dd63cf0fcb1dadd8ae56018f1f

Fix bug introduced in commit fc98ce6.  Do not create the regions file (#579)
unless the code is in diagnostic mode AND it is the total object
being processed.

drizzlepac/hlautils/catalog_utils.py
@@ -965,8 +965,9 @@ class HAPSegmentCatalog(HAPCatalogBase):
             self.kernel = self.tp_sources['segment']['kernel']
 
         # For debugging purposes only, create a "regions" files to use for ds9 overlay of the segm_img.
-        # Create the image regions file here in case there is a failure
-        if self.diagnostic_mode:
+        # Create the image regions file here in case there is a failure.  This diagnostic portion of the
+        # code should only be invoked when working on the total object catalog (self.segm_img is defined).
+        if self.diagnostic_mode and self.segm_img:
             # Copy out only the X and Y coordinates to a "diagnostic_mode table" and cast as an Astropy Table
             # so a scalar can be added to the centroid coordinates
             tbl = self.source_cat["X-Centroid", "Y-Centroid"]