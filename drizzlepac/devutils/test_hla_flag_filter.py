6b4f3e89ad4707336e9b80cac96f1dd753369951
Michael Dulude <dulude@stsci.edu>
2020-02-04 12:20:49 -0500

    drizzlepac/devutils/test_hla_flag_filter.py: initial commit

---
+++ drizzlepac/devutils/test_hla_flag_filter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""script to run hla_flag_filter and compare_sourcelists to test hla_flag_filter parameter updates"""
+
+import pdb
+import pickle
+import sys
+
+from drizzlepac import hapsequencer
+from drizzlepac.devutils.comparison_tools import compare_sourcelists
+from drizzlepac.hlautils import hla_flag_filter
+from stsci.tools import logutil
+from stwcs import wcsutil
+
+def run_stuff(input_pickle_filename)
+    hff_inputs = pickle.load( open( "input_pickle_filename", "rb" ) )
+    pdb.set_trace()
+
+# =======================================================================================================================
+if __name__ == "__main__":
+    input_pickle_filename = sys.argv[1]
+    run_stuff(input_pickle_filename)
