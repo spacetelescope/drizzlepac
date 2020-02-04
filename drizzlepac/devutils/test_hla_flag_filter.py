97a5e66e799c29fc267de651b0a9af4c3e1e8b38
Michael Dulude <dulude@stsci.edu>
2020-02-04 15:46:36 -0500

    drizzlepac/devutils/test_hla_flag_filter.py: all call to hla_flag_filter.run_source_list_flagging()

--- drizzlepac/devutils/test_hla_flag_filter.py
+++ drizzlepac/devutils/test_hla_flag_filter.py
@@ -10,9 +10,29 @@ from drizzlepac.devutils.comparison_tools import compare_sourcelists
 from drizzlepac.hlautils import hla_flag_filter
 from stsci.tools import logutil
 from stwcs import wcsutil
+# -----------------------------------------------------------------------------------------------------------
+def run_hla_flag_fitler(hff_inputs):
+    source_list = hla_flag_filter.run_source_list_flagging(hff_inputs["drizzled_image"],
+                                                           hff_inputs["flt_list"],
+                                                           hff_inputs["param_dict"],
+                                                           hff_inputs["exptime"],
+                                                           hff_inputs["plate_scale"],
+                                                           hff_inputs["median_sky"],
+                                                           hff_inputs["catalog_name"],
+                                                           hff_inputs["catalog_data"],
+                                                           hff_inputs["cat_type"],
+                                                           hff_inputs["drz_root_dir"],
+                                                           hff_inputs["hla_flag_msk"],
+                                                           hff_inputs["ci_lookup_file_path"],
+                                                           hff_inputs["output_custom_pars_file"],
+                                                           hff_inputs["log_level"],
+                                                           hff_inputs["diagnostic_mode"])
+    return source_list
+# -----------------------------------------------------------------------------------------------------------
 
 def run_stuff(input_pickle_filename):
     hff_inputs = pickle.load(open(input_pickle_filename, "rb"))
+    sourcelist = run_hla_flag_fitler(hff_inputs)
     pdb.set_trace()
 
 # =======================================================================================================================
