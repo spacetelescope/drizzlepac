c5eea5113bba90747cdf8066796ee58c114fd96f
Michael Dulude <dulude@stsci.edu>
2020-02-26 11:04:10 -0500

    drizzlepac/hapsequencer.py: fixed incorrectly name input in call to run_sourcelist_comparision().

--- drizzlepac/hapsequencer.py
+++ drizzlepac/hapsequencer.py
@@ -461,7 +461,7 @@ def run_hap_processing(input_filename, diagnostic_mode=False, use_defaults_confi
 
         # 9: Compare results to HLA classic counterparts (if possible)
         if diagnostic_mode:
-            run_sourcelist_comparision(total_list, diagnostic_mode=diagnostic_mode, log_level=log_level)
+            run_sourcelist_comparision(total_obj_list, diagnostic_mode=diagnostic_mode, log_level=log_level)
         # Write out manifest file listing all products generated during processing
         log.info("Creating manifest file {}.".format(manifest_name))
         log.info("  The manifest contains the names of products generated during processing.")
