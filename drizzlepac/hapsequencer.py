24731bc974f58ca577acd8c81ac930daf25c5a61
Michael Dulude <dulude@stsci.edu>
2020-05-08 16:44:31 -0400

    drizzlepac/hapsequencer.py: removed a couple of vestigial calls to svm_quality_analysis.py subroutines

--- drizzlepac/hapsequencer.py
+++ drizzlepac/hapsequencer.py
@@ -488,12 +488,6 @@ def run_hap_processing(input_filename, diagnostic_mode=False, use_defaults_confi
             log.info("SVM Quality Assurance statistics have been requested for this dataset, {}.".format(input_filename))
             svm_qa.run_quality_analysis(total_obj_list, log_level=log_level)
 
-            # Compute WCS differences between the primary WCS and the alternates, except for OPUS WCS
-            svm_qa.report_wcs(total_obj_list)
-
-        # 9: Compare results to HLA classic counterparts (if possible)
-        if diagnostic_mode:
-            run_sourcelist_comparision(total_obj_list, diagnostic_mode=diagnostic_mode, log_level=log_level)
         # Write out manifest file listing all products generated during processing
         log.info("Creating manifest file {}.".format(manifest_name))
         log.info("  The manifest contains the names of products generated during processing.")
