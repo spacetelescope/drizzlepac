14e37767679dccc3ac9b418ef25d0a67504a64ea
Warren Hack <hack@stsci.edu>
2020-07-20 10:46:31 -0400

    Debugged code to generate correct plots

--- drizzlepac/haputils/pipeline_graphics.py
+++ drizzlepac/haputils/pipeline_graphics.py
@@ -342,6 +342,8 @@ def build_astrometry_plots(pandas_file,
     
     fitCDS = ColumnDataSource(fit_data)
     residsCDS = ColumnDataSource(resids_data)
+    gaiafitCDS = ColumnDataSource(gaia_fit_data)
+    gaiaresidsCDS = ColumnDataSource(gaia_resids_data)
     
 
     if output_dir is not None:
@@ -352,9 +354,17 @@ def build_astrometry_plots(pandas_file,
     # Generate the astrometric plots
     astrometry_plot_name = generate_summary_plots(fitCDS, output=summary_filename)
     
-    resids_plot_names = [astrometry_plot_name]
+    resids_plot_names = [relative_plot_name]
 
-    for filename,rootname in zip(fitCDS.data['header.FILENAME'], fitCDS.data['index']):
+    # Generate plots for absolute alignment
+    catalog_name = gaiafitCDS.data[gaia_fit_columns[-1]][0].upper()
+    gaia_title = 'Alignment to {}'.format(catalog_name)
+    absolute_plot_name = generate_summary_plots(gaiafitCDS, gaia_fit_columns,
+                                                base_title=gaia_title,
+                                                  output=summary_filename)
+
+    # Generate residual plots for relative alignment
+    for filename,rootname in zip(fitCDS.data['gen_info.imgname'], fitCDS.data['index']):
         i = residsCDS.data['index'].tolist().index(rootname)
         resids_dict = {'x': residsCDS.data[resids_columns[0]][i],
                        'y': residsCDS.data[resids_columns[1]][i],
@@ -370,6 +380,26 @@ def build_astrometry_plots(pandas_file,
                                                    output_dir=output_dir,
                                                    output=output)
         resids_plot_names.append(resids_plot_name)
+
+    # Generate residual plots for alignment to GAIA
+    for filename,rootname in zip(gaiafitCDS.data['gen_info.imgname'], gaiafitCDS.data['index']):        
+        # Now add plots for fits to GAIA
+        j = gaiaresidsCDS.data['index'].tolist().index(rootname)
+        gaia_resids_dict = {'x': gaiaresidsCDS.data[gaia_resids_columns[0]][i],
+                            'y': gaiaresidsCDS.data[gaia_resids_columns[1]][i],
+                            'xr': gaiaresidsCDS.data[gaia_resids_columns[2]][i],
+                            'yr': gaiaresidsCDS.data[gaia_resids_columns[3]][i]}
+        gaia_cds = ColumnDataSource(gaia_resids_dict)
+
+        gaia_rms_x = float(gaiafitCDS.data[gaia_fit_columns[0]][i])
+        gaia_rms_y = float(gaiafitCDS.data[gaia_fit_columns[1]][i])
+        
+        resids_plot_name = generate_relative_residual_plots(gaia_cds, filename, 
+                                                   rms=(gaia_rms_x,gaia_rms_y),
+                                                   output_dir=output_dir,
+                                                   output=gaia_output)
+        resids_plot_names.append(resids_plot_name)
+        
 
     resids_plot_names = list(filter(lambda a: a != None, resids_plot_names))
     
--- drizzlepac/haputils/quality_analysis.py
+++ drizzlepac/haputils/quality_analysis.py
@@ -41,7 +41,6 @@ function myFunction() {
 
 
 """
-import json
 import sys
 from datetime import datetime
 import time
