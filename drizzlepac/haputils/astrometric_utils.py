b711177117af2fad9ec90d511035f77a4ce0bbfb
stsci-hack <hack@stsci.edu>
2020-10-15 02:37:10 -0400

    Update logic for detecting CR streaks

--- drizzlepac/haputils/astrometric_utils.py
+++ drizzlepac/haputils/astrometric_utils.py
@@ -778,7 +778,7 @@ def extract_sources(img, dqmask=None, fwhm=3.0, kernel=None, photmode=None,
     if classify:
         cat = source_properties(imgarr, segm)
         # Remove likely cosmic-rays based on central_moments classification
-        bad_srcs = np.where(classify_sources(cat) == 0)[0] + 1
+        bad_srcs = np.where(classify_sources(cat, fwhm) == 0)[0] + 1
         # Convert this bad_srcs into a segmap that can be used to update a DQ array
         dqmap = np.zeros_like(segm.data)
         for src in bad_srcs:
@@ -931,7 +931,7 @@ def extract_sources(img, dqmask=None, fwhm=3.0, kernel=None, photmode=None,
     return tbl, segm, dqmap
 
 
-def classify_sources(catalog, sources=None):
+def classify_sources(catalog, fwhm, sources=None):
     """ Convert moments_central attribute for source catalog into star/cr flag.
 
     This algorithm interprets the central_moments from the source_properties
@@ -971,10 +971,11 @@ def classify_sources(catalog, sources=None):
         x, y = np.where(moments[src] == moments[src].max())
         valid_src = (x[0] > 1) and (y[0] > 1)
         # These look for CR streaks (not delta CRs)
-        valid_width = semiminor_axis[src].value > 0.75
-        valid_elon = elon[src].value < 2
+        valid_width = semiminor_axis[src].value < (0.75 * fwhm)  # skinny source
+        valid_elon = elon[src].value > 2  # long source
+        valid_streak = valid_width and valid_elon  # long and skinny...
         # If either a delta CR or a CR streak are identified, remove it
-        if valid_src and (valid_width and valid_elon):
+        if valid_src and not valid_streak:
             srctype[src] = 1
 
     return srctype
