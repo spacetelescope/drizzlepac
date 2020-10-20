7680608f71fad9623e21ef0d1a5d6ad87aaec6c1
Michael Dulude <dulude@stsci.edu>
2020-10-20 16:36:21 -0400

    interdetector_sourcelist_crossmatch.py: incremental progress.

--- drizzlepac/devutils/comparison_tools/interdetector_sourcelist_crossmatch.py
+++ drizzlepac/devutils/comparison_tools/interdetector_sourcelist_crossmatch.py
@@ -9,7 +9,9 @@ import os
 import pdb
 import sys
 
-from astropy.table import Table
+from astropy.coordinates import SkyCoord
+from astropy.io import fits
+from astropy.stats import sigma_clipped_stats
 import numpy as np
 
 from drizzlepac.devutils.comparison_tools import compare_sourcelists
@@ -73,13 +75,7 @@ def run(sl_names, img_names, diagnostic_mode=False, log_level=logutil.logging.IN
     # 4: (diagnostic only) write out region files to check coordinate transformation
     if diagnostic_mode:
         for data_table, reg_filename in zip([ref_data, comp_data, comp_xy_in_ref_frame], ["ref_orig.reg", "comp_orig.reg", "comp_xform.reg"]):
-            f = open(reg_filename, "w")
-            for line in data_table:
-                f.write("{} {}\n".format(line[0], line[1]))
-            f.close()
-            log.info("Wrote ds9 region file {}".format(reg_filename))
-
-
+            write_region_file(data_table, reg_filename)
 
     new_comp_xy = np.stack((comp_xy_in_ref_frame[:, 0], comp_xy_in_ref_frame[:, 1]), axis=1)
     ref_xy = np.stack((ref_data['X'], ref_data['Y']), axis=1)
@@ -95,12 +91,127 @@ def run(sl_names, img_names, diagnostic_mode=False, log_level=logutil.logging.IN
         "Comparison sourcelist: {} of {} total sources matched ({} %)".format(len(matches),
                                                                               len(new_comp_xy),
                                                                               100.0 * (float(len(matches)) / float(len(new_comp_xy)))))
+    # extract indices of the matching ref and comp lines and then use them to compile lists of matched X, Y,
+    # RA and DEC for calculation of differences
     matched_lines_comp = []
     matched_lines_ref = []
     for item in matches:
         matched_lines_comp.append(item[2])
         matched_lines_ref.append(item[5])
-    pdb.set_trace()
+
+    matching_values_ref_x = ref_data['X'][[matched_lines_ref]]
+    matching_values_ref_y = ref_data['Y'][[matched_lines_ref]]
+    matching_values_ref_ra = ref_data['RA'][[matched_lines_ref]]
+    matching_values_ref_dec = ref_data['DEC'][[matched_lines_ref]]
+    matching_values_comp_x = new_comp_xy[:, 0][[matched_lines_comp]]
+    matching_values_comp_y = new_comp_xy[:, 1][[matched_lines_comp]]
+    matching_values_comp_ra = comp_ra_dec_values[:, 0][[matched_lines_comp]]
+    matching_values_comp_dec = comp_ra_dec_values[:, 1][[matched_lines_comp]]
+
+
+    # get coordinate system type from fits headers
+    ref_frame = fits.getval(img_names[0], "radesys", ext=('sci', 1)).lower()
+    comp_frame = fits.getval(img_names[1], "radesys", ext=('sci', 1)).lower()
+
+    # force RA and Dec values to be the correct type for SkyCoord() call
+    if str(type(matching_values_ref_ra)) == "<class 'astropy.table.column.Column'>":
+        matching_values_ref_ra = matching_values_ref_ra.tolist()
+    if str(type(matching_values_ref_dec)) == "<class 'astropy.table.column.Column'>":
+        matching_values_ref_dec = matching_values_ref_dec.tolist()
+    if str(type(matching_values_comp_ra)) == "<class 'astropy.table.column.Column'>":
+        matching_values_comp_ra = matching_values_comp_ra.tolist()
+    if str(type(matching_values_comp_dec)) == "<class 'astropy.table.column.Column'>":
+        matching_values_comp_dec = matching_values_comp_dec.tolist()
+
+    # convert reference and comparison RA/Dec values into SkyCoord objects
+    matching_values_ref_rd = SkyCoord(matching_values_ref_ra, matching_values_ref_dec, frame=ref_frame, unit="deg")
+    matching_values_comp_rd = SkyCoord(matching_values_comp_ra, matching_values_comp_dec, frame=comp_frame, unit="deg")
+    # convert to ICRS coord system
+    if ref_frame != "icrs":
+        matching_values_ref_rd = matching_values_ref_rd.icrs
+    if comp_frame != "icrs":
+        matching_values_comp_rd = matching_values_comp_rd.icrs
+
+    # compute differences
+    diff_x = matching_values_comp_x - matching_values_ref_x
+    diff_y = matching_values_comp_y - matching_values_ref_y
+    diff_rd = matching_values_comp_rd.separation(matching_values_ref_rd).arcsec
+
+    diff_list = [diff_x, diff_y, diff_rd]
+    title_list = ["X-axis differences", "Y-axis differences", "On-sky separation"]
+    units_list = ["HAP WFC3/UVIS pixels", "HAP WFC3/UVIS pixels", "Arcseconds"]
+    for diff_ra, title, units in zip(diff_list, title_list, units_list):
+        log.info("Comparison - reference {} statistics ({})".format(title, units))
+
+        compute_stats(diff_ra, title)
+
+# =======================================================================================================================
+def compute_stats(diff_ra, title):
+    """Compute linear statistics on specified differences
+
+    Parameters
+    ----------
+    diff_ra : numpy or astropy array
+       1xn array containing difference values that will be used to compute stats.
+
+    title : string
+       title of the differences
+
+    Returns
+    -------
+    Nothing!
+    """
+    # 'sigma' and 'iters' input values used for various np.sigma_clipped_stats() runs
+    sigma = 3
+    n_iters = 3
+    clipped_stats = sigma_clipped_stats(diff_ra, sigma=sigma, maxiters=n_iters)
+    sigma_percentages = []
+    for sig_val in [1.0, 2.0, 3.0]:
+        sigma_percentages.append((float(np.shape(np.where((diff_ra >= (clipped_stats[0] - sig_val * clipped_stats[2])) & (diff_ra <= (clipped_stats[0] + sig_val * clipped_stats[2]))))[1])/float(np.shape(diff_ra)[0])) * 100.0)
+    log.info("            Non-Clipped Statistics")
+    log.info("Non-clipped minimum........................... {}".format(np.min(diff_ra)))
+    log.info("Non-clipped maximum........................... {}".format(np.max(diff_ra)))
+    log.info("Non-clipped mean.............................. {}".format(np.mean(diff_ra)))
+    log.info("Non-clipped median............................ {}".format(np.median(diff_ra)))
+    log.info("Non-clipped standard deviation................ {}".format(np.std(diff_ra)))
+    log.info(
+        "Non-clipped mean in units of SD............... {}\n".format(np.divide(np.mean(diff_ra), np.std(diff_ra))))
+    log.info(
+        "       Sigma-clipped Statistics; \u03C3 = {}, Number of clipping steps = {}".format(sigma,
+                                                                                             n_iters))
+    log.info("Sigma-clipped mean............................ {}".format(clipped_stats[0]))
+    log.info("Sigma-clipped median.......................... {}".format(clipped_stats[1]))
+    log.info("Sigma-clipped standard deviation.............. {}".format(clipped_stats[2]))
+    for sig_val, pct_val in zip([1.0, 2.0, 3.0], sigma_percentages):
+        log.info(
+            "% all diff values within {}\u03C3 of clipped mean... {}%".format(int(sig_val), pct_val))
+    log.info("\n\n")
+    
+# =======================================================================================================================
+
+
+def write_region_file(coords, region_filename):
+    """Writes specified x, y coordinates to specified region file.
+
+    Parameters
+    ----------
+    coords : ndarray
+        list of X, Y coordinates to write
+
+    region_filename : str
+        Name of the output region file
+
+    Returns
+    -------
+    Nothing!
+    """
+    f = open(region_filename, "w")
+    for line in coords:
+        f.write("{} {}\n".format(line[0], line[1]))
+    f.close()
+    log.info("Wrote ds9 region file {}".format(region_filename))
+
+
 # =======================================================================================================================
 if __name__ == "__main__":
     # process command-line inputs with argparse
@@ -137,3 +248,7 @@ if __name__ == "__main__":
 
 
     run(user_args.sl_list, user_args.img_list, user_args.diagnostic_mode, log_level=log_level)
+
+    # TODO : subtract sigma-clipped mean off of differences, write out region files containing top 10
+    #  (25? 100? 500?) cross-matched sources, sorted by largest (absolute) mean-subtracted difference. Generate seperate
+    #  region files for X and Y, and maybe combined vector ((sqrt(x^+y^2))
