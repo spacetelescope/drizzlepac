b24af6e4874b9db4a636d589d335a85ffe264484
stsci-hack <hack@stsci.edu>
2020-11-02 14:34:04 -0500

    Update docstring to be consistent with code

--- drizzlepac/haputils/product.py
+++ drizzlepac/haputils/product.py
@@ -621,8 +621,8 @@ class SkyCellExposure(HAPProduct):
         """
             Create a copy of the original input to be renamed and used for multi-visit processing.
 
-            New exposure filename needs to follow the convention:
-            hst_skycell-p<PPPP>x<XX>y<YY>_<instr>_<detector>_<filter>-<layer>-<ipppssoo>_fl[ct].fits
+            New exposure filename needs to follow the convention (based on the SVM naming convention):
+            hst_skycell-p<PPPP>x<XX>y<YY>_<instr>_<detector>_<filter>-<layer>_<ipppssoo>_fl[ct].fits
 
             Parameters
             ----------
