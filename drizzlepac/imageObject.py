8cb276b0c262139e98a8b2ca46db31112908e82d
Warren J. Hack <hack@stsci.edu>
2020-12-03 10:46:46 -0800

    Create more specific static mask name (#888) (#889)

--- drizzlepac/imageObject.py
+++ drizzlepac/imageObject.py
@@ -980,9 +980,9 @@ class imageObject(baseImageObject):
         #self._rootname=self._image['PRIMARY'].header["ROOTNAME"]
         self._rootname=fileutil.buildNewRootname(filename)
         self.outputNames=self._setOutputNames(self._rootname)
-        self.outroot = output.split('_')[0] if output else filename.split('_')[0]
+        self.outroot = '_'.join(output.split('_')[:-1]) if output else '_'.join(filename.split('_')[:-1])
         self.outroot = self.outroot.lower()
-        
+
         # flag to indicate whether or not to write out intermediate products
         # to disk (default) or keep everything in memory
         self.inmemory = inmemory
