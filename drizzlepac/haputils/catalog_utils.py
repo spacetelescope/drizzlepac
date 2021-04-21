56df11f28e67dee36abb535050958214377f1c5f
Warren J. Hack <hack@stsci.edu>
2021-04-21 15:43:36 -0400

    Adjust threshold for PSF source finding

--- drizzlepac/haputils/catalog_utils.py
+++ drizzlepac/haputils/catalog_utils.py
@@ -935,6 +935,10 @@ class HAPPointCatalog(HAPCatalogBase):
                     # Perform manual detection of sources using theoretical PSFs
                     # Initial test data: ictj65
                     try:
+                        # Subtract the detection threshold image so that detection is anything > 0
+                        region -= - (reg_rms * self.param_dict['nsigma'])
+                        # insure no negative values for deconvolution
+                        region = np.clip(region, 0., region.max())
                         user_peaks, source_fwhm = decutils.find_point_sources(self.image.imgname,
                                                                  data=region,
                                                                  def_fwhm=source_fwhm,
