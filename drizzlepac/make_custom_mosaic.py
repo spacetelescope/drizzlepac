6ed02655ce36ed2acf5c14aeab4d374ae9148824
Michael Dulude <dulude@stsci.edu>
2021-07-14 14:26:29 -0400

    make_custom_mosaic.py: Completed argparse-driven command-line argument parsing in main().

--- drizzlepac/make_custom_mosaic.py
+++ drizzlepac/make_custom_mosaic.py
@@ -64,6 +64,23 @@ def main():
     -------
     Nothing.
     """
+    parser = argparse.ArgumentParser(description='Create custom mosaic based on user-specified images and '
+                                                 'world coordinate system (WCS) information')
+    parser.add_argument('input_images',
+                        help='Search pattern to be used to identify images to process or alternately, the '
+                             'name of a text file containing a list of images to process')
+    parser.add_argument('-i', '--output_wcs_info_source_file', required=False, default=None,
+                        help='Name of a file that contains the desired world coordinate system (WCS) '
+                             'information for the final output mosaic product(s). Users should use one of '
+                             'the following valid filetypes: 1) a calibrated fits image file (_flt.fits or '
+                             '_flc.fits fits) 2) a headerlet fits file (_hlet.fits) or 3) a text file '
+                             'containing all values of a complete WCS solution, one value per line, in the '
+                             'following specific order: CRVAL terms, CRPIX terms, Platescale value, and '
+                             'Orientation. If a source file for the output WCS information is not '
+                             'explicitly specified, the WCS information of the skycell containing the '
+                             'largest fraction of the input observations will be used.')
+
+    user_args = parser.parse_args()
 
     rv = perform()
 # ------------------------------------------------------------------------------------------------------------
