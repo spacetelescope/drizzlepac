36ad47ad0928800667c9ec647663d92642ec7c51
mdlpstsci <mdelapena@stsci.edu>
2021-11-19 08:46:11 -0500

    Added a new PyTest test_svm_j97e06  (#1189)
    * Added a new test test_svm_j97e06 which is ACS WFC, Grism and Direct image data.
    
    * Add the poller file acs_97e_06_input.out to go with its PyTest.
    
    * Modified the read of the manifest file from ascii.read to Table.read
    (both with format="no_header") in an effort to overcome the PyTest objection.
    
    * Change made only as a debug test of the Azure test system.
    
    * Another test to debug failure under Azure.
    
    * Cleared debug statements and use alternate method to read the manifest
    file (with and readlines).  It is not understood why the Table.read or
    ascii.read did not work.
    
    * Changes to test_svm_j97e06.py to work with --basetemp and added comments.
    Also changed test_svm_je281u.py and template_svm_test.py for the --basetemp
    to make all the files consistent (a but of smuggling).
    
    * Review suggests to take care to strip the end of a string of characters
    when later using the string with "endswith".
    
    * Per review - Modified to not perform an assert in a loop as this would
    pre-maturely end the processing.  Fixed failure messages.

--- tests/hap/test_svm_j97e06.py
+++ tests/hap/test_svm_j97e06.py
@@ -1,32 +1,54 @@
-""" This module tests full pipeline SVM processing for an ACS WFC, full-frame, one filter dataset.
+""" This module tests full pipeline SVM processing of ACS WFC Grism and Direct image data.
 
 """
 import datetime
 import glob
 import os
+import math
+import numpy as np
 import pytest
 
 from drizzlepac.haputils import astroquery_utils as aqutils
 from drizzlepac import runsinglehap
 from astropy.io import fits, ascii
+from astropy.table import Table
 from pathlib import Path
 
 """
-    test_svm_je281u.py
+    test_svm_j97e06.py
 
     This test file can be executed in the following manner:
-        $ pytest -s --basetemp=/internal/hladata/yourUniqueDirectoryHere test_svm_je281u.py >& je281u.log &
-        $ tail -f je281u.log
+        $ pytest -s --basetemp=/internal/hladata/yourUniqueDirectoryHere test_svm_j97e06.py >& j97e06.log &
+        $ tail -f j97e06.log
       * Note: When running this test, the `--basetemp` directory should be set to a unique
         existing directory to avoid deleting previous test output.
-      * The POLLER_FILE exists in the tests/hap directory.
-      * If running manually with `--basetemp`, the je281u.log file will still be written to the 
+      * The POLLER_FILE exists in the tests/hap directory with the PyTests.
+      * If running manually with `--basetemp`, the j97e06.log file will still be written to the 
         originating directory.
 
 """
 
-WCS_SUB_NAME = "FIT_SVM_GAIA"
-POLLER_FILE = "acs_e28_1u_input.out"
+# Expectation values used directly or indirectly for the test assert statements
+WCS_SUB_NAME = "IDC_4BB1536OJ"
+POLLER_FILE = "acs_97e_06_input.out"
+EXPECTED_POINT_SOURCES = {"wfc": 2}
+EXPECTED_SEG_SOURCES = {"wfc": 7}
+MEAN_CAT_MAGAP1_POINT = {
+"hst_10374_06_acs_wfc_f814w_j97e06_point-cat.ecsv": 18.79,
+"hst_10374_06_acs_wfc_f625w_j97e06_point-cat.ecsv": 18.86,
+"hst_10374_06_acs_wfc_f775w_j97e06_point-cat.ecsv": 18.78,
+"hst_10374_06_acs_wfc_f555w_j97e06_point-cat.ecsv": 19.05,
+"hst_10374_06_acs_wfc_f850lp_j97e06_point-cat.ecsv": 18.84,
+"hst_10374_06_acs_wfc_f606w_j97e06_point-cat.ecsv": 18.97}
+MEAN_CAT_MAGAP1_SEGMENT = {
+"hst_10374_06_acs_wfc_f850lp_j97e06_segment-cat.ecsv": 20.56,
+"hst_10374_06_acs_wfc_f555w_j97e06_segment-cat.ecsv": 21.73,
+"hst_10374_06_acs_wfc_f775w_j97e06_segment-cat.ecsv": 20.71,
+"hst_10374_06_acs_wfc_f814w_j97e06_segment-cat.ecsv": 20.73,
+"hst_10374_06_acs_wfc_f625w_j97e06_segment-cat.ecsv": 21.08,
+"hst_10374_06_acs_wfc_f606w_j97e06_segment-cat.ecsv": 21.17}
+POINT_DIFF = 0.5
+SEGMENT_DIFF = 0.5
 
 @pytest.fixture(scope="module")
 def read_csv_for_filenames():
@@ -160,17 +182,83 @@ def test_svm_wcs(gather_output_data):
     # Check the output primary WCSNAME includes FIT_SVM_GAIA as part of the string value
     tdp_files = [files for files in gather_output_data if files.lower().find("total") > -1 and files.lower().endswith(".fits")]
 
-    for tdp in tdp_files:
-        wcsname = fits.getval(tdp, "WCSNAME", ext=1).upper()
-        print("\ntest_svm_wcs.  WCSNAME: {} Output file: {}".format(wcsname, tdp))
-        assert WCS_SUB_NAME in wcsname, f"WCSNAME is not as expected for file {tdp}."
+    # For this dataset, there is only one total data product. However, make the test more general to 
+    # handle multiple total data products which all have the same simple WCSNAME.
+    wcsnames = [fits.getval(tdp, "WCSNAME", ext=1).upper() for tdp in tdp_files]
+    assert len(set(wcsnames)) == 1, f"WCSNAMES are not all the same: {wcsnames}"
 
 
-def test_svm_cat_sources(gather_output_data):
-    # Check the output catalogs should contain > 0 measured sources
-    cat_files = [files for files in gather_output_data if files.lower().endswith("-cat.ecsv")]
+def test_svm_point_cat_numsources(gather_output_data):
+   # Check that the point catalogs have the expected number of sources
+    cat_files = [files for files in gather_output_data if files.lower().endswith("point-cat.ecsv")]
 
+    num_sources = {cat:len(ascii.read(cat, format="ecsv")) for cat in cat_files}
+    valid_cats = {}
+    for cat in EXPECTED_POINT_SOURCES.keys():
+        for file in cat_files:
+            if cat in file and "total" in file:
+                valid_cats[cat] = (np.isclose(num_sources[file], EXPECTED_POINT_SOURCES[cat], rtol=0.1), num_sources[file])
+                break
+    bad_cats = [cat for cat in valid_cats if not valid_cats[cat][0]]
+    assert len(bad_cats) == 0,  f"Point Catalog(s) {bad_cats} had {valid_cats} sources, expected {EXPECTED_POINT_SOURCES}"
+
+
+def test_svm_segment_cat_numsources(gather_output_data):
+   # Check that the point catalogs have the expected number of sources
+    cat_files = [files for files in gather_output_data if files.lower().endswith("segment-cat.ecsv")]
+
+    num_sources = {cat: len(ascii.read(cat, format="ecsv")) for cat in cat_files}
+    valid_cats = {}
+    for cat in EXPECTED_SEG_SOURCES.keys():
+        for file in cat_files:
+            if cat in file and "total" in file:
+                valid_cats[cat] = (np.isclose(num_sources[file], EXPECTED_SEG_SOURCES[cat], rtol=0.1), num_sources[file])
+                break
+    bad_cats = [cat for cat in valid_cats if not valid_cats[cat][0]]
+    assert len(bad_cats) == 0, f"Segment Catalog(s) {bad_cats} had {valid_cats} sources, expected {EXPECTED_SEG_SOURCES}"
+
+
+def test_svm_point_cat_meanmag(gather_output_data):
+    cat_files = [files.lower() for files in gather_output_data if files.lower().endswith("point-cat.ecsv") and files.lower().find("total") < 0]
+
+    # Compute the mean of the MagAp1 in the filtered catalogs and do not include flagged bad data
+    Mag1_mean = {}
     for cat in cat_files:
-        table_length = len(ascii.read(cat, format="ecsv"))
-        print("\ntest_svm_cat_sources. Number of sources in catalog {} is {}.".format(cat, table_length))
-        assert table_length > 0, f"Catalog file {cat} is unexpectedly empty"
+        table = ascii.read(cat)
+        Mag1_array = table['MagAp1'].data
+        Mag1_mean[cat] = -9999.0
+        if len(Mag1_array[Mag1_array > -9999.0]) > 0:
+            Mag1_mean[cat] = Mag1_array[Mag1_array > -9999.0].mean()
+
+    good_cats = {}
+    for cat in MEAN_CAT_MAGAP1_POINT.keys():
+        for file in cat_files:
+            if cat == file:
+                good_cats[cat] = (np.isclose(MEAN_CAT_MAGAP1_POINT[cat], Mag1_mean[cat], rtol=POINT_DIFF), Mag1_mean[cat])
+                break
+
+    bad_cats = [cat for cat in good_cats if not good_cats[cat][0]]
+    assert len(bad_cats) == 0, f"Point Catalog(s) {bad_cats} had {good_cats} sources, expected {MEAN_CAT_MAGAP1_POINT}"
+
+
+def test_svm_segment_cat_meanmag(gather_output_data):
+    cat_files = [files.lower() for files in gather_output_data if files.lower().endswith("segment-cat.ecsv") and files.lower().find("total") < 0]
+
+    # Compute the mean of the MagAp1 in the filtered catalogs and do not include flagged bad data
+    Mag1_mean = {}
+    for cat in cat_files:
+        table = ascii.read(cat)
+        Mag1_array = table['MagAp1'].data
+        Mag1_mean[cat] = -9999.0
+        if len(Mag1_array[Mag1_array > -9999.0]) > 0:
+            Mag1_mean[cat] = Mag1_array[Mag1_array > -9999.0].mean()
+
+    good_cats = {}
+    for cat in MEAN_CAT_MAGAP1_SEGMENT.keys():
+        for file in cat_files:
+            if cat == file:
+                good_cats[cat] = (np.isclose(MEAN_CAT_MAGAP1_SEGMENT[cat], Mag1_mean[cat], rtol=SEGMENT_DIFF), Mag1_mean[cat])
+                break
+
+    bad_cats = [cat for cat in good_cats if not good_cats[cat][0]]
+    assert len(bad_cats) == 0, f"Segment Catalog(s) {bad_cats} had {good_cats} sources, expected {MEAN_CAT_MAGAP1_SEGMENT}"
