563113226e30e787ecf1ff9ed19045e915052e7e
Warren J. Hack <hack@stsci.edu>
2022-03-16 15:14:54 -0400

    Fix WFC3 IR WCSNAME assertion

--- tests/hap/test_svm_ibyt50.py
+++ tests/hap/test_svm_ibyt50.py
@@ -30,7 +30,7 @@ from pathlib import Path
 
 POLLER_FILE = "wfc3_byt_50_input.out"
 WCS_UVIS_SUB_NAME = "FIT_SVM_GAIA"
-WCS_IR_SUB_NAME = "FIT_SVM_GSC242"
+WCS_IR_SUB_NAME = "FIT_SVM_GAIA"
 expected_total_point_sources = {
 "hst_13023_50_wfc3_ir_total_ibyt50_point-cat.ecsv": 122,
 "hst_13023_50_wfc3_uvis_total_ibyt50_point-cat.ecsv": 105}
