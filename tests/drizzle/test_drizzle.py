bd8530eabd1b50dae340128d34bfa0df52aacc6c
Steve Goldman <32876747+s-goldman@users.noreply.github.com>
2023-04-14 15:57:51 -0400

    Working tests using the Get_Grid Class and appropriate dny

--- tests/drizzle/test_drizzle.py
+++ tests/drizzle/test_drizzle.py
@@ -4,127 +4,125 @@ from astropy import wcs
 from drizzlepac import cdriz
 
 
-class get_grid:
+# testing set up
+class Get_Grid:
     def __init__(self):
-        def get_wcs(crpix1, crpix2):
+        def get_wcs(_grid):
             w = wcs.WCS()
-            w.wcs.ctype = ["RA---CAR", "DEC--CAR"]
-            w.wcs.crpix = [crpix1, crpix2]
+            w.wcs.ctype = ["RA---TAN", "DEC--TAN"]
+            w.wcs.crpix = [_grid[0], _grid[1]]
             w.wcs.crval = [10, 10]
             w.wcs.cdelt = [1e-3, 1e-3]
             w.wcs.set()
             return w
 
-        self.insci = np.ones((50, 60), dtype=np.float32)
-        self.inwht = np.ones((50, 60), dtype=np.float32)
-        self.outsci = np.zeros((51, 66), dtype=np.float32)
-        self.outwht = np.ones((51, 66), dtype=np.float32)
-        self.outctx = np.ones((51, 66), dtype=np.int32)
-        self.w1 = get_wcs(50, 60)
-        self.w2 = get_wcs(51, 66)
-        self.mapping = cdriz.DefaultWCSMapping(self.w1, self.w2, 51, 66, 1)
+        np.random.seed(0)  # keep same random across each instance
+        in_grid = (50, 60)
+        out_grid = (51, 66)
+        self.insci = np.random.randn(in_grid[0], in_grid[1]).astype("float32")
+        self.inwht = np.ones(in_grid, dtype=np.float32)
+        self.dny = out_grid[1]  # output y_grid
+        self.outsci = np.zeros(out_grid, dtype=np.float32)
+        self.outwht = np.ones(out_grid, dtype=np.float32)
+        self.outctx = np.ones(out_grid, dtype=np.int32)
+        self.w1 = get_wcs(in_grid)
+        self.w2 = get_wcs(out_grid)
+        self.mapping = cdriz.DefaultWCSMapping(
+            self.w1, self.w2, out_grid[0], out_grid[1], 1
+        )
 
 
+def cdriz_call(_set_point_function, kernel):
+    cdriz.tdriz(
+        _set_point_function.insci,
+        _set_point_function.inwht,
+        _set_point_function.outsci,
+        _set_point_function.outwht,
+        _set_point_function.outctx,
+        1,  # uniqid
+        0,  # ystart
+        1,
+        1,
+        _set_point_function.dny,  # _dny
+        1.0,  # pix_ratio
+        1.0,
+        1.0,
+        "center",
+        1.0,  # pixfrac
+        kernel,
+        "cps",  # units
+        1.0,  # expscale
+        1.0,  # wt_scale
+        "INDEF",  # fillval
+        0,  # nmiss
+        0,  # nskip
+        1,
+        _set_point_function.mapping,
+    )
+
+
+def save_array(_data, _name):
+    np.savetxt(
+        _name,
+        X=_data,
+        fmt="%1.8f",
+        delimiter=",",
+    )
+
+
+# ########### TESTS
 @pytest.fixture
 def point_function():
-    return get_grid()
+    return Get_Grid()
+
+
+
+@pytest.mark.parametrize("kernel", ["square", "point", "turbo", "gaussian", "lanczos3"])
+def test_point_kernel(kernel, point_function, new_truth=False):
+    """Function tests different c code point kernels (inputs already created on instantiation).
+
+    Parameters
+    ----------
+    kernel : str
+        String associated with one of the c code point kernel options.
+    point_function : Class
+        The Class inintialized in Get_Class which includes all of the inputs need to run cdriz.tdriz.
+    new_truth : bool, optional
+        Flag, whether to save a new csv truth file from the output data, by default False
+    """
+
+    # add missing/flagged pixels in inwht
+    point_function.insci[20:22, 21:22] = 100
+
+    # resample:
+    cdriz_call(point_function, kernel)
+
+    if new_truth:
+        # save new truth file
+        save_array(point_function.outsci, f"./tests/drizzle/{kernel}_truth.csv")
+
+    truth_array = np.genfromtxt(f"./tests/drizzle/{kernel}_truth.csv", delimiter=",")
+    assert np.allclose(point_function.outsci, truth_array, atol=1e-4)
 
 
 @pytest.mark.parametrize("kernel", ["square", "point", "turbo", "gaussian", "lanczos3"])
 def test_zero_input_weight(kernel, point_function):
-    """
-    Test do_driz with missing pixels
+    """Tests that do_driz ignores bad pixels, or those that have an input weight (inwht) of 0. 
+
+    Parameters
+    ----------
+    kernel : str
+        String associated with one of the c code point kernel options.
+    point_function : Class
+        The Class inintialized in Get_Class which includes all of the inputs need to run cdriz.tdriz.
     """
     # add missing/flagged pixels in inwht
     point_function.inwht[20:21, 22:23] = 0
 
-    # resample:
-    cdriz.tdriz(
-        point_function.insci,
-        point_function.inwht,
-        point_function.outsci,
-        point_function.outwht,
-        point_function.outctx,
-        1, # uniqid
-        0, # ystart
-        1,
-        1,
-        200, #_dny
-        1.0, # pix_ratio
-        1.0,
-        1.0,
-        "center",
-        1.0, # pixfrac
-        kernel,
-        "cps", # units
-        1.0, # expscale
-        1.0, # wt_scale
-        "INDEF", # fillval
-        0, # nmiss
-        0, # nskip
-        1,
-        point_function.mapping,
-    )             
+    # resample
+    cdriz_call(point_function, kernel)
 
     # check that no pixel with 0 weight has any counts:
     assert np.allclose(
         np.sum(np.abs(point_function.outsci[(point_function.outwht == 0)])), 0
     )
-
-#test kernel response function
-@pytest.mark.parametrize("kernel", ["square", "point", "turbo", "gaussian", "lanczos3"])
-def test_point_kernel(kernel, point_function):
-    """
-    Test do_driz kernel with point
-    """
-    # add missing/flagged pixels in inwht
-    point_function.insci[20:21, 21:22] = 50
-    # resample:
-    cdriz.tdriz(
-        point_function.insci,
-        point_function.inwht,
-        point_function.outsci,
-        point_function.outwht,
-        point_function.outctx,
-        1,
-        0,
-        1,
-        1,
-        200,
-        1.0,
-        1.0,
-        1.0,
-        "center",
-        1.0,
-        kernel,
-        "cps",
-        1.0,
-        1.0,
-        "INDEF",
-        0,
-        0,
-        1,
-        point_function.mapping,
-    )
-    # for generating truth files
-    np.savetxt('./tests/drizzle/'+kernel+'_truth.csv', X=point_function.outsci, delimiter=',')
-    import matplotlib.pyplot as plt
-    fig = plt.figure(figsize=(4,2))
-    ax1 = fig.add_subplot(111, projection=point_function.w1)
-    ax1.imshow(point_function.outsci, origin='lower', cmap='Greys')
-    ax1.set_ylabel(' ')
-    ax1.set_xlabel(' ')
-    fig.savefig('./tests/drizzle/'+kernel+'_truth.png') 
-
-    #for generating input file
-    np.savetxt('./tests/drizzle/input_truth.csv', X=point_function.insci, fmt='%.32e',delimiter=',')
-    fig = plt.figure(figsize=(4,2))
-    ax1 = fig.add_subplot(111, projection=point_function.w1)
-    ax1.imshow(point_function.insci, origin='lower', cmap='Greys')
-    ax1.set_ylabel(' ')
-    ax1.set_xlabel(' ')
-    fig.savefig('./tests/drizzle/input_truth.png')    
-
-    truth_array = np.genfromtxt('./tests/drizzle/'+kernel+'_truth.csv', delimiter=',')
-
-    assert np.allclose(point_function.outsci, truth_array, atol=1e-4)
