79fa5b87634d505fa6aefb3a2f08241cae90bfed
Steve Goldman <32876747+s-goldman@users.noreply.github.com>
2023-04-27 11:31:29 -0400

    Tests working

--- tests/drizzle/test_cdriz.py
+++ tests/drizzle/test_cdriz.py
@@ -6,14 +6,14 @@ import cdriz_setup
 @pytest.fixture
 def kernel_pars():
     # get cdriz.triz inputs from cdriz_setup.py
-    _params = cdriz_setup.Get_Grid(inx=5, iny=5, outx=10, outy=10)
+    _params = cdriz_setup.Get_Grid(inx=10, iny=10, outx=13, outy=13)
     _params.zero_background()
     return _params
 
 
 # "square", "point", "turbo", "gaussian", "lanczos3"
 @pytest.mark.parametrize("kernel", ["square", "point", "turbo"])
-def test_square_kernel(kernel_pars, kernel, return_png=True):
+def test_square_kernel(kernel_pars, kernel, return_png=False):
     """Function tests different c code point kernels (inputs already created on instantiation).
 
     Parameters
@@ -40,7 +40,7 @@ def test_square_kernel(kernel_pars, kernel, return_png=True):
     assert np.allclose(np.sum(kernel_pars.outsci), 10000, 1e-7)
 
 
-def test_gaussian_kernel(kernel_pars, return_png=True):
+def test_gaussian_kernel(kernel_pars, return_png=False):
     """Same as above test but with gaussian kernel."""
     kernel_pars.insci[3, 3] = 1e4
     cdriz_setup.cdriz_call(kernel_pars, "gaussian")
@@ -48,10 +48,10 @@ def test_gaussian_kernel(kernel_pars, return_png=True):
         cdriz_setup.generate_png(
             kernel_pars, "./tests/drizzle/truth_files/cdriz_gaussian.png"
         )
-    assert np.allclose(np.sum(kernel_pars.outsci), 10058.42, 1e-3)
+    assert np.allclose(np.sum(kernel_pars.outsci), 10000, 1e-3)
 
 
-def test_lanczos3_kernel(kernel_pars, return_png=True):
+def test_lanczos3_kernel(kernel_pars, return_png=False):
     """Same as above test but with lanczos3 kernel."""
     kernel_pars.insci[3, 3] = 1e4
     cdriz_setup.cdriz_call(kernel_pars, "lanczos3")
@@ -59,4 +59,4 @@ def test_lanczos3_kernel(kernel_pars, return_png=True):
         cdriz_setup.generate_png(
             kernel_pars, "./tests/drizzle/truth_files/cdriz_lanczos3.png"
         )
-    assert np.allclose(np.sum(kernel_pars.outsci), 1E4, 1e-3)
+    assert np.allclose(np.sum(kernel_pars.outsci), 9882.103, 1e-3)
