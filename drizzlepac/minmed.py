029935f1ebae6ed0c95180b5e5c8bdc8a76e891b
Mihai Cara <mcara@users.noreply.github.com>
2023-06-26 09:00:44 -0400

    Fix an incompatibility with np 1.25 in minmed (#1573)

--- drizzlepac/minmed.py
+++ drizzlepac/minmed.py
@@ -421,7 +421,7 @@ def min_med(images, weight_images, readnoise_list, exptime_list,
     images = np.asarray(images)
     weight_images = np.asarray(weight_images)
 
-    if weight_masks == [] or weight_masks is None:
+    if weight_masks is None or np.size(weight_masks) == 0:
         weight_masks = None
         mask_sum = np.zeros(images.shape[1:], dtype=np.int16)
         all_bad_idx = np.array([], dtype=int)
