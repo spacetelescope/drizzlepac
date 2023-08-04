a9b19a5174ba9e94ab370c4bbe4a4ac4f812c8a3
Michele De La Pena <mdelapena@stsci.edu>
2023-08-04 16:12:19 -0400

    Remove skip from updated imageObject test

--- tests/test_imageObject.py
+++ tests/test_imageObject.py
@@ -14,21 +14,22 @@ def forall_cases(cases):
     return decorate
 
 
-@pytest.mark.xfail(reason="Broken")
+#@pytest.mark.xfail(reason="Broken")
 class TestimageObject():
     """test the implementation of imageObject by creating and examining an object
        from an image file thats on disk
     """
 
-    #not sure if this is the correct way to do it
+    def noFilename(self, filename):
+        image=imageObject.imageObject(filename)
     def test_NoFilename(self,filename=''):
-        self.assertRaises(IOError,imageObject.imageObect,filename)
+        with pytest.raises(IOError):
+            self.noFilename(filename)
 
     def test_Attributes(self, filename="./j8uq10lbq_flt.fits"):
         image=imageObject.imageObject(filename)
 
         #just check to make sure the global attributes are not empty
         assert(image._filename != '')
-        assert(image._naxis1 > 0)
-        assert(image._naxis2 > 0)
+        assert(image._rootname != '')
         assert(image._instrument != '')
