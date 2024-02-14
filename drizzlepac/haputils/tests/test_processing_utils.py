ad1e93828a29c85801ee05eb89c6a3d1a72fa2cd
Steve Goldman <32876747+s-goldman@users.noreply.github.com>
2024-02-14 11:23:52 -0500

    added comments.

--- drizzlepac/haputils/tests/test_processing_utils.py
+++ drizzlepac/haputils/tests/test_processing_utils.py
@@ -4,22 +4,22 @@ import shutil
 from astropy.io import fits
 from drizzlepac.haputils import processing_utils
 
-abs_path = "drizzlepac/haputils/tests/"
-
+test_path = "drizzlepac/haputils/tests/"
 
 @pytest.mark.parametrize(
     "filename",
     [
-        "sample_ipsoot_flc.fits",
-        "sample_ipsoot_flt.fits",
-        "sample_svm_flc.fits",
-        "sample_svm_flt.fits",
+        "sample_ipsoot_flc.fits", # WFC3/UVIS
+        "sample_ipsoot_flt.fits", # WFC3/IR
+        "sample_svm_flc.fits", # WFC3/UVIS SVM file
+        "sample_svm_flt.fits", # WFC3/IR SVM file
         "sample_ipsoot_flt_w_skycell.fits"
     ],
 )
 def test_add_skycell_to_header(filename, tmpdir):
     temp_path = os.path.join(tmpdir, f"temp_{filename}")
-    shutil.copy2(abs_path+filename, temp_path)
+    # creating copy as to not alter the sample file. 
+    shutil.copy2(test_path+filename, temp_path)
     processing_utils.add_skycell_to_header(temp_path)
     hdu = fits.open(temp_path)
     assert hdu[1].header["SKYCELL"] == "p0121x12y16"
