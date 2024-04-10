5f2cbfb41cf04e9290cb8b9bf74890a1a8d289a4
Steve Goldman <32876747+s-goldman@users.noreply.github.com>
2024-04-10 15:23:47 -0400

    moved warnings to processInput due to duplication per exposure.

--- drizzlepac/processInput.py
+++ drizzlepac/processInput.py
@@ -131,6 +131,23 @@ def setCommonInput(configObj, createOutwcs=True, overwrite_dict={}):
         # Update configObj with values from mpars
         cfgpars.mergeConfigObj(configObj, mdriztab_dict)
     
+    # add warnings for kernels that may not work as intended
+    kernels_w_warnings = ["gaussian", "lanczos3", "tophat"]
+    for kernel_param in overwrite_dict:
+        if overwrite_dict[kernel_param] in kernels_w_warnings:
+            if overwrite_dict[kernel_param] == "tophat":
+                log.warning(
+                    f'WARNING: It was found that kernel "{overwrite_dict[kernel_param]}" does not work as intended. '
+                    + f'It has been removed as a option. Using kernel "square" instead for {kernel_param}.'
+                )
+                overwrite_dict[kernel_param] = "square"
+            else:
+                log.warning(
+                    f'WARNING: We cannot guarantee that kernel "{overwrite_dict[kernel_param]}" is properly conserving flux.'
+                    + "Make sure you understand the effects of using this kernel, "
+                    + 'or use one of other available options: "square", "point", or "turbo".'
+                )
+                
     if overwrite_dict:
         # user inputs are removed in above line, we need to add them back
         # overwrite values in configObj with those from the input_dict
