e7e4763b94d349b753bd97070122714f6eec3d5b
Steve Goldman <32876747+s-goldman@users.noreply.github.com>
2025-10-01 13:41:34 -0400

    changed source of files.

--- tests/hap/test_align.py
+++ tests/hap/test_align.py
@@ -79,7 +79,7 @@ class TestAlignMosaic(BaseHLATest):
         # Since these are full file names (*_flc.fits) which cannot be obtained via astroquery from
         # MAST, get the data now using ci_watson.
         for input_file in input_filenames:
-            get_bigdata("hst-hla-pipeline", "dev", "mosaic_ngc188", input_file)
+            get_bigdata("drizzlepac", "dev", "hap," "mosaic_ngc188", input_file)
 
         dataset_table = alignimages.perform_align(
             input_filenames,
@@ -124,7 +124,7 @@ class TestAlignMosaic(BaseHLATest):
         # Since these are full file names (*_flc.fits) which cannot be obtained via astroquery from
         # MAST, get the data now using ci_watson.
         for input_file in input_filenames:
-            get_bigdata("hst-hla-pipeline", "dev", "mosaic_47tuc", input_file)
+            get_bigdata("drizzlepac", "dev", "hap," "mosaic_47tuc", input_file)
 
         dataset_table = alignimages.perform_align(
             input_filenames,
@@ -212,7 +212,7 @@ class TestAlignMosaic(BaseHLATest):
         # Since these are full file names (*_flc.fits) which cannot be obtained via astroquery from
         # MAST, get the data now using ci_watson.
         for input_file in input_filenames:
-            get_bigdata("hst-hla-pipeline", "dev", "base_tests", input_file)
+            get_bigdata("drizzlepac", "dev", "hap," "base_tests", input_file)
 
         dataset_table = alignimages.perform_align(
             input_filenames,
@@ -279,7 +279,7 @@ class TestAlignMosaic(BaseHLATest):
         # Since these are full file names (*_flc.fits) which cannot be obtained via astroquery from
         # MAST, get the data now using ci_watson.
         for input_file in input_filenames:
-            get_bigdata("hst-hla-pipeline", "dev", "base_tests", input_file)
+            get_bigdata("drizzlepac", "dev", "hap," "base_tests", input_file)
 
         dataset_table = alignimages.perform_align(
             input_filenames,
