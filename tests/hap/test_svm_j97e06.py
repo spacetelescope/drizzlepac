3997a6c34443718cceec08b0a91c574d8be0f85c
Steve Goldman <32876747+s-goldman@users.noreply.github.com>
2025-11-25 13:58:52 -0500

    added bigdata to svm regression tests, moved common code to svm_test_utils.

--- tests/hap/test_svm_j97e06.py
+++ tests/hap/test_svm_j97e06.py
@@ -1,18 +1,16 @@
 """ This module tests full pipeline SVM processing of ACS WFC Grism and Direct image data.
 
 """
-import datetime
-import glob
-import os
-import math
 import numpy as np
 import pytest
 
-from drizzlepac.haputils import astroquery_utils as aqutils
 from drizzlepac import runsinglehap
 from astropy.io import fits, ascii
-from astropy.table import Table
 from pathlib import Path
+
+from . import svm_test_utils as svm_utils
+
+pytestmark = pytest.mark.bigdata
 
 """
     test_svm_j97e06.py
@@ -52,119 +50,28 @@ SEGMENT_DIFF = 0.5
 
 @pytest.fixture(scope="module")
 def read_csv_for_filenames():
-    # Read the CSV poller file residing in the tests directory to extract the individual visit FLT/FLC filenames
-    path = os.path.join(os.path.dirname(__file__), POLLER_FILE)
-    table = ascii.read(path, format="no_header")
-    filename_column = table.colnames[0]
-    filenames = list(table[filename_column])
-    print("\nread_csv_for_filenames. Filesnames from poller: {}".format(filenames))
-
-    return filenames
+    return svm_utils.load_poller_filenames(POLLER_FILE)
 
 
 @pytest.fixture(scope="module")
 def gather_data_for_processing(read_csv_for_filenames, tmp_path_factory):
-    # Create working directory specified for the test
-    curdir = tmp_path_factory.mktemp(os.path.basename(__file__)) 
-    os.chdir(curdir)
-
-    # Establish FLC/FLT lists and obtain the requested data
-    flc_flag = ""
-    flt_flag = ""
-    # In order to obtain individual FLC or FLT images from MAST (if the files are not reside on disk) which
-    # may be part of an ASN, use only IPPPSS with a wildcard.  The unwanted images have to be removed
-    # after-the-fact.
-    for fn in read_csv_for_filenames:
-        if fn.lower().endswith("flc.fits") and flc_flag == "":
-            flc_flag = fn[0:6] + "*"
-        elif fn.lower().endswith("flt.fits") and flt_flag == "":
-            flt_flag = fn[0:6] + "*"
-     
-        # If both flags have been set, then break out the loop early.  It may be
-        # that all files have to be checked which means the for loop continues
-        # until its natural completion.
-        if flc_flag and flt_flag:
-            break
-
-    # Get test data through astroquery - only retrieve the pipeline processed FLC and/or FLT files
-    # (e.g., j*_flc.fits) as necessary. The logic here and the above for loop is an attempt to
-    # avoid downloading too many images which are not needed for processing.
-    flcfiles = []
-    fltfiles = []
-    if flc_flag:
-        flcfiles = aqutils.retrieve_observation(flc_flag, suffix=["FLC"], product_type="pipeline")
-    if flt_flag:
-        fltfiles = aqutils.retrieve_observation(flt_flag, suffix=["FLT"], product_type="pipeline")
-
-    flcfiles.extend(fltfiles)
-
-    # Keep only the files which exist in BOTH lists for processing
-    files_to_process= set(read_csv_for_filenames).intersection(set(flcfiles))
-
-    # Identify unwanted files from the download list and remove from disk
-    files_to_remove = set(read_csv_for_filenames).symmetric_difference(set(flcfiles))
-    try:
-        for ftr in files_to_remove:
-           os.remove(ftr)
-    except Exception as x_cept:
-        print("")
-        print("Exception encountered: {}.".format(x_cept))
-        print("The file {} could not be deleted from disk. ".format(ftr))
-        print("Remove files which are not used for processing from disk manually.")
-
-    print("\ngather_data_for_processing. Gathered data: {}".format(files_to_process))
-
-    return files_to_process
+    svm_utils.change_to_temp_working_dir(tmp_path_factory, __file__)
+    return svm_utils.retrieve_data_for_processing(read_csv_for_filenames)
 
 
 @pytest.fixture(scope="module")
 def gather_output_data(construct_manifest_filename):
-    # Determine the filenames of all the output files from the manifest
-    files = []
-    with open(construct_manifest_filename, 'r') as fout:
-        for line in fout.readlines():
-            files.append(line.rstrip("\n"))
-    print("\ngather_output_data. Output data files: {}".format(files))
-
-    return files
+    return svm_utils.read_manifest(construct_manifest_filename)
 
 
 @pytest.fixture(scope="module")
 def construct_manifest_filename(read_csv_for_filenames):
-    # Construct the output manifest filename from input file keywords
-    inst = fits.getval(read_csv_for_filenames[0], "INSTRUME", ext=0).lower()
-    root = fits.getval(read_csv_for_filenames[0], "ROOTNAME", ext=0).lower()
-    tokens_tuple = (inst, root[1:4], root[4:6], "manifest.txt")
-    manifest_filename = "_".join(tokens_tuple)
-    print("\nconstruct_manifest_filename. Manifest filename: {}".format(manifest_filename))
-
-    return manifest_filename
+    return svm_utils.build_manifest_name(read_csv_for_filenames[0])
 
 
 @pytest.fixture(scope="module", autouse=True)
 def svm_setup(gather_data_for_processing):
-    # Act: Process the input data by executing runsinglehap - time consuming activity
-
-    current_dt = datetime.datetime.now()
-    print(str(current_dt))
-    print("\nsvm_setup fixture")
-
-    # Read the "poller file" and download the input files, as necessary
-    input_names = gather_data_for_processing
-
-    # Run the SVM processing
-    path = os.path.join(os.path.dirname(__file__), POLLER_FILE)
-    try:
-        status = runsinglehap.perform(path)
-
-    # Catch anything that happens and report it.  This is meant to catch unexpected errors and
-    # generate sufficient output exception information so algorithmic problems can be addressed.
-    except Exception as except_details:
-        print(except_details)
-        pytest.fail("\nsvm_setup. Exception Visit: {}\n", path)
-
-    current_dt = datetime.datetime.now()
-    print(str(current_dt))
+    svm_utils.run_svm_pipeline(POLLER_FILE, runsinglehap.perform)
 
 
 # TESTS
